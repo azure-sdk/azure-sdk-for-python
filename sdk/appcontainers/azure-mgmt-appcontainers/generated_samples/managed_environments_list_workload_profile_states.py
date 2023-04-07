3cf11ed83799d7a248634a5857d3fe28c2b78ff5
SDKAuto <sdkautomation@microsoft.com>
2023-04-07 03:07:10 +0000

    CodeGen from PR 23476 in Azure/azure-rest-api-specs Merge f8c5ea9d1206c2838ef1fb7ffa48bc7cd56917c5 into cdb52946818dd48bcfe87a54a7c0953f33264ecb

--- sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/managed_environments_list_workload_profile_states.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/managed_environments_list_workload_profile_states.py
@@ -37,6 +37,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2022-10-01/examples/ManagedEnvironments_ListWorkloadProfileStates.json
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2023-04-01-preview/examples/ManagedEnvironments_ListWorkloadProfileStates.json
 if __name__ == "__main__":
     main()
