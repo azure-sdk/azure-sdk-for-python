b12f68059db497a271252327d4729255259e0e1c
SDKAuto <sdkautomation@microsoft.com>
2023-04-12 05:16:11 +0000

    CodeGen from PR 23520 in Azure/azure-rest-api-specs Merge e6de9ccf0d2bfe944fb3cde13df829bc0cae63f6 into 97c5c70de4e3b2e97cf85b707f5f6b5832c5a082

--- sdk/redisenterprise/azure-mgmt-redisenterprise/generated_samples/redis_enterprise_check_name_availability.py
+++ sdk/redisenterprise/azure-mgmt-redisenterprise/generated_samples/redis_enterprise_check_name_availability.py
@@ -14,7 +14,7 @@ from azure.mgmt.redisenterprise import RedisEnterpriseManagementClient
     pip install azure-identity
     pip install azure-mgmt-redisenterprise
 # USAGE
-    python redis_enterprise_delete_private_endpoint_connection.py
+    python redis_enterprise_check_name_availability.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,14 +29,12 @@ def main():
         subscription_id="subid",
     )
 
-    response = client.private_endpoint_connections.begin_delete(
-        resource_group_name="rg1",
-        cluster_name="cache1",
-        private_endpoint_connection_name="pectest01",
-    ).result()
+    response = client.redis_enterprise.check_name_availability(
+        parameters={"name": "cacheName", "type": "Microsoft.Cache/redisenterprise"},
+    )
     print(response)
 
 
-# x-ms-original-file: specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples/RedisEnterpriseDeletePrivateEndpointConnection.json
+# x-ms-original-file: specification/redisenterprise/resource-manager/Microsoft.Cache/preview/2023-03-01-preview/examples/RedisEnterpriseCheckNameAvailability.json
 if __name__ == "__main__":
     main()
