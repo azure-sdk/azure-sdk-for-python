209ca3bd8acf58c10ac1d8820014bc9233c2e922
SDKAuto <sdkautomation@microsoft.com>
2023-04-27 09:35:14 +0000

    CodeGen from PR 23743 in Azure/azure-rest-api-specs Merge 4519fae5923b804e7710f31ac487d6274b0eb27e into 4d35f0d0da0c702944acb61b2f2f4d7657586235

--- sdk/hdinsight/azure-mgmt-hdinsight/generated_samples/enable_linux_cluster_azure_monitor.py
+++ sdk/hdinsight/azure-mgmt-hdinsight/generated_samples/enable_linux_cluster_azure_monitor.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/stable/2021-06-01/examples/EnableLinuxClusterAzureMonitor.json
+# x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/preview/2023-04-15-preview/examples/EnableLinuxClusterAzureMonitor.json
 if __name__ == "__main__":
     main()
