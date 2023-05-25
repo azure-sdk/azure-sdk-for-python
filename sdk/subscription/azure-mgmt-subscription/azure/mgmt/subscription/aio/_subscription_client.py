406a1cfbef88a1dd945e7a75e263a1d940099168
SDKAuto <sdkautomation@microsoft.com>
2023-05-25 18:06:56 +0000

    CodeGen from PR 24171 in Azure/azure-rest-api-specs Merge 29e55ba44ca7c06b8c07ab4a524971d880b086e8 into be63fad20943ff5f1cdf9849d2631839b683935a

--- sdk/subscription/azure-mgmt-subscription/azure/mgmt/subscription/aio/_subscription_client.py
+++ sdk/subscription/azure-mgmt-subscription/azure/mgmt/subscription/aio/_subscription_client.py
@@ -60,7 +60,7 @@ class SubscriptionClient:  # pylint: disable=client-accepts-api-version-keyword,
         self, credential: "AsyncTokenCredential", base_url: str = "https://management.azure.com", **kwargs: Any
     ) -> None:
         self._config = SubscriptionClientConfiguration(credential=credential, **kwargs)
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -105,5 +105,5 @@ class SubscriptionClient:  # pylint: disable=client-accepts-api-version-keyword,
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
