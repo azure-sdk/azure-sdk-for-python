52a2ea5669bc95aba536a4e183166be7db3777c5
SDKAuto <sdkautomation@microsoft.com>
2023-06-20 22:24:12 +0000

    CodeGen from PR 2932 in test-repo-billy/azure-rest-api-specs Merge b008490ad1523b72a66bb01c970b7b4f7bb61a23 into e872e6afc9002037e5d83f09ba26ee8710c0900e

--- sdk/databox/azure-mgmt-databox/azure/mgmt/databox/v2021_08_01_preview/_data_box_management_client.py
+++ sdk/databox/azure-mgmt-databox/azure/mgmt/databox/v2021_08_01_preview/_data_box_management_client.py
@@ -62,9 +62,13 @@ class DataBoxManagementClient(
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
-        self.jobs = JobsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.service = ServiceOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.operations = Operations(
+            self._client, self._config, self._serialize, self._deserialize, "2021-08-01-preview"
+        )
+        self.jobs = JobsOperations(self._client, self._config, self._serialize, self._deserialize, "2021-08-01-preview")
+        self.service = ServiceOperations(
+            self._client, self._config, self._serialize, self._deserialize, "2021-08-01-preview"
+        )
 
     def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
--- sdk/databox/azure-mgmt-databox/azure/mgmt/databox/v2022_02_01/_data_box_management_client.py
+++ sdk/databox/azure-mgmt-databox/azure/mgmt/databox/v2022_02_01/_data_box_management_client.py
@@ -62,9 +62,9 @@ class DataBoxManagementClient(
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
-        self.jobs = JobsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.service = ServiceOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize, "2022-02-01")
+        self.jobs = JobsOperations(self._client, self._config, self._serialize, self._deserialize, "2022-02-01")
+        self.service = ServiceOperations(self._client, self._config, self._serialize, self._deserialize, "2022-02-01")
 
     def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
