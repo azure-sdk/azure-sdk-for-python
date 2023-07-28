76b8365e04dc430ea90c8ed7ae3967371fe61be0
SDKAuto <sdkautomation@microsoft.com>
2023-07-28 07:30:18 +0000

    CodeGen from PR 25026 in Azure/azure-rest-api-specs Merge 068edffd4531e88deb0bd70b061cf9595c901f0d into fe2113ec4be9d8d52929eee0583d3c841ba3c189

--- sdk/servicenetworking/azure-mgmt-servicenetworking/generated_samples/traffic_controller_patch.py
+++ sdk/servicenetworking/azure-mgmt-servicenetworking/generated_samples/traffic_controller_patch.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/TrafficControllerPatch.json
+# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/preview/2023-05-01-preview/examples/TrafficControllerPatch.json
 if __name__ == "__main__":
     main()
