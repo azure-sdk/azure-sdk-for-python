0435e80e967b07c897b3f09389bb778d8a6e890b
SDKAuto <sdkautomation@microsoft.com>
2023-08-01 18:52:40 +0000

    CodeGen from PR 25070 in Azure/azure-rest-api-specs Merge f98bed7e98d1e8493c30562b331dd482dabce34d into 04926ec6b467c7e94edd212c9baaa6c1fa820f11

--- sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/list_operations.py
+++ sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/list_operations.py
@@ -33,6 +33,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/ListOperations.json
+# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2023-04-01/examples/ListOperations.json
 if __name__ == "__main__":
     main()
--- sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/patch_management_group.py
+++ sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/patch_management_group.py
@@ -38,6 +38,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/PatchManagementGroup.json
+# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2023-04-01/examples/PatchManagementGroup.json
 if __name__ == "__main__":
     main()
