de099fddb1836dd29d1f8c7bb2edb5a2c2f4a26b
SDKAuto <sdkautomation@microsoft.com>
2023-08-07 07:40:07 +0000

    CodeGen from PR 25197 in Azure/azure-rest-api-specs Merge c008a71aebeef195b8ac95fab078ae9d667fe8d2 into a3089b2764df11b10a8a43e2a5a33dbe4d3653ca

--- sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alert_processing_rules_create_or_update_add_correlation.py
+++ sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alert_processing_rules_create_or_update_add_correlation.py
@@ -14,7 +14,7 @@ from azure.mgmt.alertsmanagement import AlertsManagementClient
     pip install azure-identity
     pip install azure-mgmt-alertsmanagement
 # USAGE
-    python alert_processing_rules_create_or_update_remove_all_action_groups_recurring_maintenance_window.py
+    python alert_processing_rules_create_or_update_add_correlation.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -31,32 +31,22 @@ def main():
 
     response = client.alert_processing_rules.create_or_update(
         resource_group_name="alertscorrelationrg",
-        alert_processing_rule_name="RemoveActionGroupsRecurringMaintenance",
+        alert_processing_rule_name="CorrelateAlerts",
         alert_processing_rule={
             "location": "Global",
             "properties": {
-                "actions": [{"actionType": "RemoveAllActionGroups"}],
-                "conditions": [
+                "actions": [
                     {
-                        "field": "TargetResourceType",
-                        "operator": "Equals",
-                        "values": ["microsoft.compute/virtualmachines"],
+                        "actionType": "CorrelateAlerts",
+                        "correlateBy": [{"field": "essentials.alertRule"}],
+                        "correlationInterval": "PT30M",
+                        "notificationsForCorrelatedAlerts": "SuppressAlways",
+                        "priority": 50,
                     }
                 ],
-                "description": "Remove all ActionGroups from all Vitual machine Alerts during the recurring maintenance",
+                "description": "Correlate Alerts Example.",
                 "enabled": True,
-                "schedule": {
-                    "recurrences": [
-                        {
-                            "daysOfWeek": ["Saturday", "Sunday"],
-                            "endTime": "04:00:00",
-                            "recurrenceType": "Weekly",
-                            "startTime": "22:00:00",
-                        }
-                    ],
-                    "timeZone": "India Standard Time",
-                },
-                "scopes": ["/subscriptions/subId1/resourceGroups/RGId1", "/subscriptions/subId1/resourceGroups/RGId2"],
+                "scopes": ["/subscriptions/subId1"],
             },
             "tags": {},
         },
@@ -64,6 +54,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples/AlertProcessingRules_Create_or_update_remove_all_action_groups_recurring_maintenance_window.json
+# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2023-05-01-preview/examples/AlertProcessingRules_Create_or_update_add_correlation.json
 if __name__ == "__main__":
     main()
