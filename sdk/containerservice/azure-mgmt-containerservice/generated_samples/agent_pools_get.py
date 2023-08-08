48425358c08c3c279fb62dd8f94e788bd59164ff
SDKAuto <sdkautomation@microsoft.com>
2023-08-08 23:27:15 +0000

    CodeGen from PR 25180 in Azure/azure-rest-api-specs Merge 5dde31538c336fbd0b4d229dc5542b832e8740a5 into db24ef8bfbf6b214310af0872a923130e27ae1b6

--- sdk/containerservice/azure-mgmt-containerservice/generated_samples/agent_pools_get.py
+++ sdk/containerservice/azure-mgmt-containerservice/generated_samples/agent_pools_get.py
@@ -26,17 +26,17 @@ from azure.mgmt.containerservice import ContainerServiceClient
 def main():
     client = ContainerServiceClient(
         credential=DefaultAzureCredential(),
+        agent_pool_name="agentpool1",
         subscription_id="subid1",
     )
 
     response = client.agent_pools.get(
         resource_group_name="rg1",
         resource_name="clustername1",
-        agent_pool_name="agentpool1",
     )
     print(response)
 
 
-# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-07-01/examples/AgentPoolsGet.json
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2023-07-02-preview/examples/AgentPoolsGet.json
 if __name__ == "__main__":
     main()
