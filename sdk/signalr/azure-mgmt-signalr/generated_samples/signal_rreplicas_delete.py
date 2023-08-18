16ea8afb04f175f3b52c31ad105abefb1b97faa4
SDKAuto <sdkautomation@microsoft.com>
2023-08-18 05:58:39 +0000

    CodeGen from PR 25378 in Azure/azure-rest-api-specs Merge 658fb5c422a8caaffe413286e71c6a1a7722187d into 624dbc769880e5676ae8bb20d3c82ebd1783c64a

--- sdk/signalr/azure-mgmt-signalr/generated_samples/signal_rreplicas_delete.py
+++ sdk/signalr/azure-mgmt-signalr/generated_samples/signal_rreplicas_delete.py
@@ -14,7 +14,7 @@ from azure.mgmt.signalr import SignalRManagementClient
     pip install azure-identity
     pip install azure-mgmt-signalr
 # USAGE
-    python signal_r_list_by_resource_group.py
+    python signal_rreplicas_delete.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,13 +29,13 @@ def main():
         subscription_id="00000000-0000-0000-0000-000000000000",
     )
 
-    response = client.signal_r.list_by_resource_group(
+    client.signal_rreplicas.delete(
         resource_group_name="myResourceGroup",
+        resource_name="mySignalRService",
+        replica_name="mySignalRService-eastus",
     )
-    for item in response:
-        print(item)
 
 
-# x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalR_ListByResourceGroup.json
+# x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalRReplicas_Delete.json
 if __name__ == "__main__":
     main()
