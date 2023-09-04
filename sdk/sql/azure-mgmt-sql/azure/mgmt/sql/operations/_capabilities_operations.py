afb3c4e1ff1bdaa05b535035115295fc16d1e904
SDKAuto <sdkautomation@microsoft.com>
2023-09-04 03:59:55 +0000

    CodeGen from PR 25637 in Azure/azure-rest-api-specs Merge 28d8bfecb497380c53fbe4dbc57a38d12df3c67f into ea87e77e1caf628278b07b51e16650946422edff

--- sdk/sql/azure-mgmt-sql/azure/mgmt/sql/operations/_capabilities_operations.py
+++ sdk/sql/azure-mgmt-sql/azure/mgmt/sql/operations/_capabilities_operations.py
@@ -44,7 +44,7 @@ def build_list_by_location_request(
     _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
     _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-    api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2020-11-01-preview"))
+    api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-05-01-preview"))
     accept = _headers.pop("Accept", "application/json")
 
     # Construct URL
@@ -117,7 +117,7 @@ class CapabilitiesOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2020-11-01-preview"))
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))
         cls: ClsType[_models.LocationCapabilities] = kwargs.pop("cls", None)
 
         request = build_list_by_location_request(
