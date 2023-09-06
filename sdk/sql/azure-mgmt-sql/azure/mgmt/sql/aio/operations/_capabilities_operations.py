f558be69b43d4f2b63c55431631f65868b84dac1
SDKAuto <sdkautomation@microsoft.com>
2023-09-06 22:54:47 +0000

    CodeGen from PR 24902 in Azure/azure-rest-api-specs Merge 58314138cf0848f932edd13b068ec5449bb7961b into ea87e77e1caf628278b07b51e16650946422edff

--- sdk/sql/azure-mgmt-sql/azure/mgmt/sql/aio/operations/_capabilities_operations.py
+++ sdk/sql/azure-mgmt-sql/azure/mgmt/sql/aio/operations/_capabilities_operations.py
@@ -79,7 +79,7 @@ class CapabilitiesOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2020-11-01-preview"))
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))
         cls: ClsType[_models.LocationCapabilities] = kwargs.pop("cls", None)
 
         request = build_list_by_location_request(
--- sdk/sql/azure-mgmt-sql/generated_samples/create_or_update_job_private_endpoint.py
+++ sdk/sql/azure-mgmt-sql/generated_samples/create_or_update_job_private_endpoint.py
@@ -14,7 +14,7 @@ from azure.mgmt.sql import SqlManagementClient
     pip install azure-identity
     pip install azure-mgmt-sql
 # USAGE
-    python server_communication_link_list.py
+    python create_or_update_job_private_endpoint.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,14 +29,20 @@ def main():
         subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    response = client.server_communication_links.list_by_server(
-        resource_group_name="sqlcrudtest-7398",
-        server_name="sqlcrudtest-4645",
-    )
-    for item in response:
-        print(item)
+    response = client.job_private_endpoints.begin_create_or_update(
+        resource_group_name="group1",
+        server_name="server1",
+        job_agent_name="agent1",
+        private_endpoint_name="endpoint1",
+        parameters={
+            "properties": {
+                "targetServerAzureResourceId": "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/group1/providers/Microsoft.Sql/servers/targetserver1"
+            }
+        },
+    ).result()
+    print(response)
 
 
-# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/stable/2014-04-01/examples/ServerCommunicationLinkList.json
+# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-05-01-preview/examples/CreateOrUpdateJobPrivateEndpoint.json
 if __name__ == "__main__":
     main()
