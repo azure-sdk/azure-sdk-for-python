52e736b4580d229d25c381a9cff9ef41b97a36b6
SDKAuto <sdkautomation@microsoft.com>
2023-09-12 08:01:27 +0000

    CodeGen from PR 25638 in Azure/azure-rest-api-specs Merge 5b3828ab4c63528cde51e8d3f4ff4314966fb0fb into 81e979278deab6229a7b30a89dd83aac92234a3f

--- sdk/sql/azure-mgmt-sql/generated_samples/list_operations.py
+++ sdk/sql/azure-mgmt-sql/generated_samples/list_operations.py
@@ -14,7 +14,7 @@ from azure.mgmt.sql import SqlManagementClient
     pip install azure-identity
     pip install azure-mgmt-sql
 # USAGE
-    python distributed_availability_groups_create.py
+    python list_operations.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -26,25 +26,14 @@ from azure.mgmt.sql import SqlManagementClient
 def main():
     client = SqlManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id="00000000-1111-2222-3333-444444444444",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.distributed_availability_groups.begin_create_or_update(
-        resource_group_name="testrg",
-        managed_instance_name="testcl",
-        distributed_availability_group_name="dag",
-        parameters={
-            "properties": {
-                "primaryAvailabilityGroupName": "BoxLocalAg1",
-                "secondaryAvailabilityGroupName": "testcl",
-                "sourceEndpoint": "TCP://SERVER:7022",
-                "targetDatabase": "testdb",
-            }
-        },
-    ).result()
-    print(response)
+    response = client.operations.list()
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsCreate.json
+# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-05-01-preview/examples/ListOperations.json
 if __name__ == "__main__":
     main()
