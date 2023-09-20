ef1977440eb180649b87e9f46109beac9c4a6aab
SDKAuto <sdkautomation@microsoft.com>
2023-09-20 21:18:54 +0000

    CodeGen from PR 25553 in Azure/azure-rest-api-specs Merge f3e2d6c707776bdb665c95719f3a8bc164bfc62c into e770272bb303e5a9776195112ac23a80344c0b18

--- sdk/sql/azure-mgmt-sql/generated_samples/managed_database_sensitivity_labels_list_by_database.py
+++ sdk/sql/azure-mgmt-sql/generated_samples/managed_database_sensitivity_labels_list_by_database.py
@@ -14,7 +14,7 @@ from azure.mgmt.sql import SqlManagementClient
     pip install azure-identity
     pip install azure-mgmt-sql
 # USAGE
-    python distributed_availability_groups_create.py
+    python managed_database_sensitivity_labels_list_by_database.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,22 +29,15 @@ def main():
         subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    response = client.distributed_availability_groups.begin_create_or_update(
-        resource_group_name="testrg",
-        managed_instance_name="testcl",
-        distributed_availability_group_name="dag",
-        parameters={
-            "properties": {
-                "primaryAvailabilityGroupName": "BoxLocalAg1",
-                "secondaryAvailabilityGroupName": "testcl",
-                "sourceEndpoint": "TCP://SERVER:7022",
-                "targetDatabase": "testdb",
-            }
-        },
-    ).result()
-    print(response)
+    response = client.managed_database_sensitivity_labels.list_by_database(
+        resource_group_name="myRG",
+        managed_instance_name="myManagedInstanceName",
+        database_name="myDatabase",
+    )
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsCreate.json
+# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-05-01-preview/examples/ManagedDatabaseSensitivityLabelsListByDatabase.json
 if __name__ == "__main__":
     main()
