808d0576d9db1928076ac048b8de4e822591fcfe
SDKAuto <sdkautomation@microsoft.com>
2023-09-27 00:38:56 +0000

    CodeGen from PR 25979 in Azure/azure-rest-api-specs Merge d0823ea6a23bbc2f87932c03f311b08b8c20ef5f into e835d100693865cc5243cc73b4a05354ade05bdf

--- sdk/sql/azure-mgmt-sql/generated_samples/managed_instance_long_term_retention_policy_delete.py
+++ sdk/sql/azure-mgmt-sql/generated_samples/managed_instance_long_term_retention_policy_delete.py
@@ -14,7 +14,7 @@ from azure.mgmt.sql import SqlManagementClient
     pip install azure-identity
     pip install azure-mgmt-sql
 # USAGE
-    python server_communication_link_delete.py
+    python managed_instance_long_term_retention_policy_delete.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,13 +29,15 @@ def main():
         subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    client.server_communication_links.delete(
-        resource_group_name="sqlcrudtest-7398",
-        server_name="sqlcrudtest-4645",
-        communication_link_name="link1",
-    )
+    response = client.managed_instance_long_term_retention_policies.begin_delete(
+        resource_group_name="testResourceGroup",
+        managed_instance_name="testInstance",
+        database_name="testDatabase",
+        policy_name="default",
+    ).result()
+    print(response)
 
 
-# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/stable/2014-04-01/examples/ServerCommunicationLinkDelete.json
+# x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-02-01-preview/examples/ManagedInstanceLongTermRetentionPolicyDelete.json
 if __name__ == "__main__":
     main()
