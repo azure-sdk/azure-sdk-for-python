de7bf0500b92e41ca4a16a59ea4e880e5720c539
SDKAuto <sdkautomation@microsoft.com>
2023-10-05 22:33:05 +0000

    CodeGen from PR 26102 in Azure/azure-rest-api-specs Merge 2a6a713905b89d6377cbc7de59ee50b8b5ca19d8 into be3cd97ad8822cfa8c85891ce8d52ecd90ce2d7c

--- sdk/contentsafety/azure-ai-contentsafety/setup.py
+++ sdk/contentsafety/azure-ai-contentsafety/setup.py
@@ -6,66 +6,26 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 # coding: utf-8
-
-import os
-import re
 from setuptools import setup, find_packages
 
 
 PACKAGE_NAME = "azure-ai-contentsafety"
-PACKAGE_PPRINT_NAME = "Azure AI Content Safety"
-
-# a-b-c => a/b/c
-package_folder_path = PACKAGE_NAME.replace("-", "/")
-
-# Version extraction inspired from 'requests'
-with open(os.path.join(package_folder_path, "_version.py"), "r") as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError("Cannot find version information")
-
-
+version = "1.0.0b1"
 setup(
     name=PACKAGE_NAME,
     version=version,
-    description="Microsoft {} Client Library for Python".format(PACKAGE_PPRINT_NAME),
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    author="Microsoft Corporation",
-    author_email="azpysdkhelp@microsoft.com",
-    url="https://github.com/Azure/azure-sdk-for-python/tree/main/sdk",
+    description="azure-ai-contentsafety",
+    author_email="",
+    url="",
     keywords="azure, azure sdk",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-    ],
-    zip_safe=False,
-    packages=find_packages(
-        exclude=[
-            "tests",
-            # Exclude packages that will be covered by PEP420 or nspkg
-            "azure",
-            "azure.ai",
-        ]
-    ),
+    packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "azure.ai.contentsafety": ["py.typed"],
-    },
     install_requires=[
         "isodate<1.0.0,>=0.6.1",
-        "azure-core<2.0.0,>=1.24.0",
+        "azure-core<2.0.0,>=1.28.0",
         "typing-extensions>=4.3.0; python_version<'3.8.0'",
     ],
-    python_requires=">=3.7",
+    long_description="""\
+    Analyze harmful content.
+    """,
 )
