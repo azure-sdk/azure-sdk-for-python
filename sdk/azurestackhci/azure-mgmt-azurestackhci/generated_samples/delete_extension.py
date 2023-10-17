696f29969e34ec8f4802482de4bf6be309be057f
SDKAuto <sdkautomation@microsoft.com>
2023-10-17 12:42:38 +0000

    CodeGen from PR 26239 in Azure/azure-rest-api-specs Merge 7f985c612af7fffcca98e5881d9553cc7fa37c28 into 3bae4e510063fbd777b88ea5eee03c41644bc9da

--- sdk/azurestackhci/azure-mgmt-azurestackhci/generated_samples/delete_extension.py
+++ sdk/azurestackhci/azure-mgmt-azurestackhci/generated_samples/delete_extension.py
@@ -29,15 +29,14 @@ def main():
         subscription_id="fd3c3665-1729-4b7b-9a38-238e83b0f98b",
     )
 
-    response = client.extensions.begin_delete(
+    client.extensions.begin_delete(
         resource_group_name="test-rg",
         cluster_name="myCluster",
         arc_setting_name="default",
         extension_name="MicrosoftMonitoringAgent",
     ).result()
-    print(response)
 
 
-# x-ms-original-file: specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/preview/2021-09-01-preview/examples/DeleteExtension.json
+# x-ms-original-file: specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/preview/2023-08-01-preview/examples/DeleteExtension.json
 if __name__ == "__main__":
     main()
