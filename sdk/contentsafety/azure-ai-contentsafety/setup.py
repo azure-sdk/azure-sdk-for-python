ebb22f994646f296ccdeb6a87d10d93f9ba84c32
SDKAuto <sdkautomation@microsoft.com>
2023-10-18 03:08:44 +0000

    CodeGen from PR 26258 in Azure/azure-rest-api-specs Merge 62f12cede3268fc90f26e0246052cc4e30c38c31 into 3d59aae5716739a772ed9f1c5376f0d9005e1c4e

--- sdk/contentsafety/azure-ai-contentsafety/setup.py
+++ sdk/contentsafety/azure-ai-contentsafety/setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 
 PACKAGE_NAME = "azure-ai-contentsafety"
-PACKAGE_PPRINT_NAME = "Azure AI Content Safety"
+PACKAGE_PPRINT_NAME = "Azure Ai Contentsafety"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace("-", "/")
@@ -38,7 +38,7 @@ setup(
     url="https://github.com/Azure/azure-sdk-for-python/tree/main/sdk",
     keywords="azure, azure sdk",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
@@ -60,11 +60,11 @@ setup(
     ),
     include_package_data=True,
     package_data={
-        "azure.ai.contentsafety": ["py.typed"],
+        "pytyped": ["py.typed"],
     },
     install_requires=[
         "isodate<1.0.0,>=0.6.1",
-        "azure-core<2.0.0,>=1.24.0",
+        "azure-core<2.0.0,>=1.28.0",
         "typing-extensions>=4.3.0; python_version<'3.8.0'",
     ],
     python_requires=">=3.7",
