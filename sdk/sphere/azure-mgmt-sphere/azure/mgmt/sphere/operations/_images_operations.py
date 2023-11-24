7e45b51c420f1abfa983d5f8b9cf2e64b5ca39a9
SDKAuto <sdkautomation@microsoft.com>
2023-11-24 01:29:01 +0000

    CodeGen from PR 26687 in Azure/azure-rest-api-specs Merge 28cc407c07ebe4ba999ee8500a4051881e52a86e into ae577864ae37f5f0b793b6012c034e104043c2ae

--- sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/operations/_images_operations.py
+++ sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/operations/_images_operations.py
@@ -30,7 +30,7 @@ from azure.mgmt.core.polling.arm_polling import ARMPolling
 
 from .. import models as _models
 from .._serialization import Serializer
-from .._vendor import _convert_request, _format_url_section
+from .._vendor import _convert_request
 
 T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
@@ -69,7 +69,7 @@ def build_list_by_catalog_request(
         "catalogName": _SERIALIZER.url("catalog_name", catalog_name, "str", pattern=r"^[A-Za-z0-9_-]{1,50}$"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     if filter is not None:
@@ -111,7 +111,7 @@ def build_get_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
@@ -146,7 +146,7 @@ def build_create_or_update_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
@@ -182,7 +182,7 @@ def build_delete_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
