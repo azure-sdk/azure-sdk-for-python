ca0f434758aafedf958787e2ff4c18a5187a8d88
SDKAuto <sdkautomation@microsoft.com>
2023-12-06 09:48:58 +0000

    CodeGen from PR 26697 in Azure/azure-rest-api-specs Merge f717ff43bca6f21e32f0309f311ee08612e10ec5 into d402f685809d6d08be9c0b45065cadd7d78ab870

--- sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/container_apps_create_or_update.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/container_apps_create_or_update.py
@@ -31,7 +31,7 @@ def main():
 
     response = client.container_apps.begin_create_or_update(
         resource_group_name="rg",
-        container_app_name="testcontainerApp0",
+        container_app_name="testcontainerapp0",
         container_app_envelope={
             "location": "East US",
             "properties": {
@@ -46,6 +46,10 @@ def main():
                         "logLevel": "debug",
                     },
                     "ingress": {
+                        "additionalPortMappings": [
+                            {"external": True, "targetPort": 1234},
+                            {"exposedPort": 3456, "external": False, "targetPort": 2345},
+                        ],
                         "clientCertificateMode": "accept",
                         "corsPolicy": {
                             "allowCredentials": True,
@@ -84,7 +88,7 @@ def main():
                         ],
                         "stickySessions": {"affinity": "sticky"},
                         "targetPort": 3000,
-                        "traffic": [{"label": "production", "revisionName": "testcontainerApp0-ab1234", "weight": 100}],
+                        "traffic": [{"label": "production", "revisionName": "testcontainerapp0-ab1234", "weight": 100}],
                     },
                     "maxInactiveRevisions": 10,
                     "service": {"type": "redis"},
@@ -93,8 +97,8 @@ def main():
                 "template": {
                     "containers": [
                         {
-                            "image": "repo/testcontainerApp0:v1",
-                            "name": "testcontainerApp0",
+                            "image": "repo/testcontainerapp0:v1",
+                            "name": "testcontainerapp0",
                             "probes": [
                                 {
                                     "httpGet": {
@@ -113,9 +117,9 @@ def main():
                         {
                             "args": ["-c", "while true; do echo hello; sleep 10;done"],
                             "command": ["/bin/sh"],
-                            "image": "repo/testcontainerApp0:v4",
+                            "image": "repo/testcontainerapp0:v4",
                             "name": "testinitcontainerApp0",
-                            "resources": {"cpu": 0.2, "memory": "100Mi"},
+                            "resources": {"cpu": 0.5, "memory": "1Gi"},
                         }
                     ],
                     "scale": {
@@ -130,6 +134,8 @@ def main():
                     },
                     "serviceBinds": [
                         {
+                            "clientType": "dotnet",
+                            "customizedKeys": {"DesiredKey": "defaultKey"},
                             "name": "redisService",
                             "serviceId": "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rg/providers/Microsoft.App/containerApps/redisService",
                         }
@@ -142,6 +148,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2023-05-01/examples/ContainerApps_CreateOrUpdate.json
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2023-08-01-preview/examples/ContainerApps_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
