c0adb37d78eefb4f6236e594fd88f8ddc3e94c59
SDKAuto <sdkautomation@microsoft.com>
2023-12-19 01:59:03 +0000

    CodeGen from PR 26945 in Azure/azure-rest-api-specs Merge 294817fbe84583143ef5af6eb484159e02f2b19f into 2ce81a458caf18e18eafb6a55dfcc9671ec5a7d9

--- sdk/maps/azure-mgmt-maps/azure/mgmt/maps/aio/operations/_accounts_operations.py
+++ sdk/maps/azure-mgmt-maps/azure/mgmt/maps/aio/operations/_accounts_operations.py
@@ -666,7 +666,7 @@ class AccountsOperations:
 
         #. Create or have an existing User Assigned Managed Identity in the same Azure region as the
         account.
-        #. Create or update an Azure Map account with the same Azure region as the User Assigned
+        #. Create or update an Azure Maps account with the same Azure region as the User Assigned
         Managed Identity is placed.
 
         :param resource_group_name: The name of the resource group. The name is case insensitive.
@@ -703,7 +703,7 @@ class AccountsOperations:
 
         #. Create or have an existing User Assigned Managed Identity in the same Azure region as the
         account.
-        #. Create or update an Azure Map account with the same Azure region as the User Assigned
+        #. Create or update an Azure Maps account with the same Azure region as the User Assigned
         Managed Identity is placed.
 
         :param resource_group_name: The name of the resource group. The name is case insensitive.
@@ -738,7 +738,7 @@ class AccountsOperations:
 
         #. Create or have an existing User Assigned Managed Identity in the same Azure region as the
         account.
-        #. Create or update an Azure Map account with the same Azure region as the User Assigned
+        #. Create or update an Azure Maps account with the same Azure region as the User Assigned
         Managed Identity is placed.
 
         :param resource_group_name: The name of the resource group. The name is case insensitive.
