62a87e23511da5dfeb9b2fa1c2e830027a6e59f0
SDKAuto <sdkautomation@microsoft.com>
2024-01-23 19:24:52 +0000

    CodeGen from PR 27231 in Azure/azure-rest-api-specs Merge 2ad42fd7e29b479860d529772e7de570c5402628 into f9f1a6a760d39d508b85c27ef88e0434ae001044

--- sdk/nginx/azure-mgmt-nginx/generated_samples/configurations_analysis.py
+++ sdk/nginx/azure-mgmt-nginx/generated_samples/configurations_analysis.py
@@ -14,7 +14,7 @@ from azure.mgmt.nginx import NginxManagementClient
     pip install azure-identity
     pip install azure-mgmt-nginx
 # USAGE
-    python operations_list.py
+    python configurations_analysis.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -26,14 +26,17 @@ from azure.mgmt.nginx import NginxManagementClient
 def main():
     client = NginxManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id="SUBSCRIPTION_ID",
+        subscription_id="00000000-0000-0000-0000-000000000000",
     )
 
-    response = client.operations.list()
-    for item in response:
-        print(item)
+    response = client.configurations.analysis(
+        resource_group_name="myResourceGroup",
+        deployment_name="myDeployment",
+        configuration_name="default",
+    )
+    print(response)
 
 
-# x-ms-original-file: specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-09-01/examples/Operations_List.json
+# x-ms-original-file: specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-09-01/examples/Configurations_Analysis.json
 if __name__ == "__main__":
     main()
