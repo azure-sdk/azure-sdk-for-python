bccae9aefaedc9bd5eafc2f5aa10d39d150cb9a2
SDKAuto <sdkautomation@microsoft.com>
2024-02-03 01:20:37 +0000

    CodeGen from PR 27641 in Azure/azure-rest-api-specs Merge e1561238ae7f6f1d19790bc57421851fa1fd5e1d into f2ccc768574c2d24acd222d8dc530f217490a0ff

--- sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/operations/_images_operations.py
+++ sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/operations/_images_operations.py
@@ -30,7 +30,7 @@ from azure.mgmt.core.polling.arm_polling import ARMPolling
 
 from .. import models as _models
 from .._serialization import Serializer
-from .._vendor import _convert_request, _format_url_section
+from .._vendor import _convert_request
 
 T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
@@ -69,7 +69,7 @@ def build_list_by_catalog_request(
         "catalogName": _SERIALIZER.url("catalog_name", catalog_name, "str", pattern=r"^[A-Za-z0-9_-]{1,50}$"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     if filter is not None:
@@ -111,7 +111,7 @@ def build_get_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
@@ -146,7 +146,7 @@ def build_create_or_update_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
@@ -182,7 +182,7 @@ def build_delete_request(
         "imageName": _SERIALIZER.url("image_name", image_name, "str"),
     }
 
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
+    _url: str = _url.format(**path_format_arguments)  # type: ignore
 
     # Construct parameters
     _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
@@ -332,7 +332,7 @@ class ImagesOperations:
         :type resource_group_name: str
         :param catalog_name: Name of catalog. Required.
         :type catalog_name: str
-        :param image_name: Image name. Use .default for image creation. Required.
+        :param image_name: Image name. Use an image GUID for GA versions of the API. Required.
         :type image_name: str
         :keyword callable cls: A custom type or function that will be passed the direct response
         :return: Image or the result of cls(response)
@@ -484,7 +484,7 @@ class ImagesOperations:
         :type resource_group_name: str
         :param catalog_name: Name of catalog. Required.
         :type catalog_name: str
-        :param image_name: Image name. Use .default for image creation. Required.
+        :param image_name: Image name. Use an image GUID for GA versions of the API. Required.
         :type image_name: str
         :param resource: Resource create parameters. Required.
         :type resource: ~azure.mgmt.sphere.models.Image
@@ -522,7 +522,7 @@ class ImagesOperations:
         :type resource_group_name: str
         :param catalog_name: Name of catalog. Required.
         :type catalog_name: str
-        :param image_name: Image name. Use .default for image creation. Required.
+        :param image_name: Image name. Use an image GUID for GA versions of the API. Required.
         :type image_name: str
         :param resource: Resource create parameters. Required.
         :type resource: IO
@@ -558,7 +558,7 @@ class ImagesOperations:
         :type resource_group_name: str
         :param catalog_name: Name of catalog. Required.
         :type catalog_name: str
-        :param image_name: Image name. Use .default for image creation. Required.
+        :param image_name: Image name. Use an image GUID for GA versions of the API. Required.
         :type image_name: str
         :param resource: Resource create parameters. Is either a Image type or a IO type. Required.
         :type resource: ~azure.mgmt.sphere.models.Image or IO
@@ -693,7 +693,7 @@ class ImagesOperations:
         :type resource_group_name: str
         :param catalog_name: Name of catalog. Required.
         :type catalog_name: str
-        :param image_name: Image name. Use .default for image creation. Required.
+        :param image_name: Image name. Use an image GUID for GA versions of the API. Required.
         :type image_name: str
         :keyword callable cls: A custom type or function that will be passed the direct response
         :keyword str continuation_token: A continuation token to restart a poller from a saved state.
