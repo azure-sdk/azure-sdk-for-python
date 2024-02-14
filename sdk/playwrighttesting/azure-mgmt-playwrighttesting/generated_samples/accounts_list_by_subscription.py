9eb2c527d4ae6795870b08458fb47868295d789f
SDKAuto <sdkautomation@microsoft.com>
2024-02-14 00:57:19 +0000

    CodeGen from PR 27767 in Azure/azure-rest-api-specs Merge 52aa4b1bcedff239e6c68f87021ec906c3762409 into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/playwrighttesting/azure-mgmt-playwrighttesting/generated_samples/accounts_list_by_subscription.py
+++ sdk/playwrighttesting/azure-mgmt-playwrighttesting/generated_samples/accounts_list_by_subscription.py
@@ -34,6 +34,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2023-10-01-preview/examples/Accounts_ListBySubscription.json
+# x-ms-original-file: specification/playwrighttesting/resource-manager/Microsoft.AzurePlaywrightService/preview/2024-02-01-preview/examples/Accounts_ListBySubscription.json
 if __name__ == "__main__":
     main()
