6f2a5dd8eb2f67e73ee48781bf8b7aab0f11fca7
SDKAuto <sdkautomation@microsoft.com>
2024-02-14 00:57:49 +0000

    CodeGen from PR 27767 in Azure/azure-rest-api-specs Merge 52aa4b1bcedff239e6c68f87021ec906c3762409 into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/databox/azure-mgmt-databox/generated_samples/jobs_patch_system_assigned_to_user_assigned.py
+++ sdk/databox/azure-mgmt-databox/generated_samples/jobs_patch_system_assigned_to_user_assigned.py
@@ -59,6 +59,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2022-12-01/examples/JobsPatchSystemAssignedToUserAssigned.json
+# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2023-03-01/examples/JobsPatchSystemAssignedToUserAssigned.json
 if __name__ == "__main__":
     main()
