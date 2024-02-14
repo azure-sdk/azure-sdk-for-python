eb909761c4b2d9776ab403ccae3922a0f2b73939
SDKAuto <sdkautomation@microsoft.com>
2024-02-14 00:45:46 +0000

    CodeGen from PR 27767 in Azure/azure-rest-api-specs Merge 52aa4b1bcedff239e6c68f87021ec906c3762409 into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/quantum/azure-mgmt-quantum/generated_samples/quantum_workspaces_patch.py
+++ sdk/quantum/azure-mgmt-quantum/generated_samples/quantum_workspaces_patch.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/quantum/resource-manager/Microsoft.Quantum/preview/2022-01-10-preview/examples/quantumWorkspacesPatch.json
+# x-ms-original-file: specification/quantum/resource-manager/Microsoft.Quantum/preview/2023-11-13-preview/examples/quantumWorkspacesPatch.json
 if __name__ == "__main__":
     main()
