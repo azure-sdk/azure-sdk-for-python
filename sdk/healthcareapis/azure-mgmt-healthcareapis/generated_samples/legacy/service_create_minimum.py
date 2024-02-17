7ae73a18224ac362d3cb65d9686d5c81b9725514
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:15:15 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/legacy/service_create_minimum.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/legacy/service_create_minimum.py
@@ -42,6 +42,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples/legacy/ServiceCreateMinimum.json
+# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-12-01/examples/legacy/ServiceCreateMinimum.json
 if __name__ == "__main__":
     main()
