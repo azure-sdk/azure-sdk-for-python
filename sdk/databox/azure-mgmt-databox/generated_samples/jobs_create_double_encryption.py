8c4b5da927150a08d6f16e9c4491b9246978082e
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:13:38 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/databox/azure-mgmt-databox/generated_samples/jobs_create_double_encryption.py
+++ sdk/databox/azure-mgmt-databox/generated_samples/jobs_create_double_encryption.py
@@ -71,6 +71,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2022-12-01/examples/JobsCreateDoubleEncryption.json
+# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2023-03-01/examples/JobsCreateDoubleEncryption.json
 if __name__ == "__main__":
     main()
