904348fef9bce7ce48db45b806c64618d7a5451c
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:15:31 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/databox/azure-mgmt-databox/generated_samples/validate_address_post.py
+++ sdk/databox/azure-mgmt-databox/generated_samples/validate_address_post.py
@@ -49,6 +49,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2022-12-01/examples/ValidateAddressPost.json
+# x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2023-03-01/examples/ValidateAddressPost.json
 if __name__ == "__main__":
     main()
