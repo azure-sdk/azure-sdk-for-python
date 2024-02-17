f83c8c1ce418653392ee519c2fe67d8d27355122
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:17:50 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/legacy/service_delete.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/legacy/service_delete.py
@@ -35,6 +35,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples/legacy/ServiceDelete.json
+# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-12-01/examples/legacy/ServiceDelete.json
 if __name__ == "__main__":
     main()
