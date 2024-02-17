b55c6b0b06d7591b0abd492cb1d6cab07269aa91
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:14:27 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/elastic/azure-mgmt-elastic/azure/mgmt/elastic/aio/operations/_all_traffic_filters_operations.py
+++ sdk/elastic/azure-mgmt-elastic/azure/mgmt/elastic/aio/operations/_all_traffic_filters_operations.py
@@ -58,8 +58,8 @@ class AllTrafficFiltersOperations:
 
         Get the list of all traffic filters for the account.
 
-        :param resource_group_name: The name of the resource group to which the Elastic resource
-         belongs. Required.
+        :param resource_group_name: The name of the resource group. The name is case insensitive.
+         Required.
         :type resource_group_name: str
         :param monitor_name: Monitor resource name. Required.
         :type monitor_name: str
