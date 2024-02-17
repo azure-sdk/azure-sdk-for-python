c547fd4d814bc2fa6c423ae53f1d38b0a010075c
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 04:24:32 +0000

    CodeGen from PR 27815 in Azure/azure-rest-api-specs Merge db844bde6c76e6fbcdbf2b53a0a1639b46a3225c into 1ded9f239c9985888070b3f9767277d381f30d2e

--- sdk/elastic/azure-mgmt-elastic/azure/mgmt/elastic/aio/operations/_detach_traffic_filter_operations.py
+++ sdk/elastic/azure-mgmt-elastic/azure/mgmt/elastic/aio/operations/_detach_traffic_filter_operations.py
@@ -111,8 +111,8 @@ class DetachTrafficFilterOperations:
 
         Detach traffic filter for the given deployment.
 
-        :param resource_group_name: The name of the resource group to which the Elastic resource
-         belongs. Required.
+        :param resource_group_name: The name of the resource group. The name is case insensitive.
+         Required.
         :type resource_group_name: str
         :param monitor_name: Monitor resource name. Required.
         :type monitor_name: str
