4c515e45c8d527e9aff742749ac7b58a0688922c
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 03:13:53 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/databoxedge/azure-mgmt-databoxedge/generated_samples/delete_monitoring_config.py
+++ sdk/databoxedge/azure-mgmt-databoxedge/generated_samples/delete_monitoring_config.py
@@ -29,12 +29,11 @@ def main():
         subscription_id="4385cf00-2d3a-425a-832f-f4285b1c9dce",
     )
 
-    response = client.monitoring_config.begin_delete(
+    client.monitoring_config.begin_delete(
         device_name="testedgedevice",
         role_name="testrole",
         resource_group_name="GroupForEdgeAutomation",
     ).result()
-    print(response)
 
 
 # x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/DeleteMonitoringConfig.json
