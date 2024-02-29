ad753610954e85bb52889497542b5065d8d3d994
SDKAuto <sdkautomation@microsoft.com>
2024-02-29 00:13:28 +0000

    CodeGen from PR 27861 in Azure/azure-rest-api-specs Merge d6fcc46341f274b8af42a4cdcfa14e1f8d472619 into f428eaf0bbda62a0a5449ff16f7bb88a16411e33

--- sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/privatelink/workspace_private_link_resource_get.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/privatelink/workspace_private_link_resource_get.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples/privatelink/WorkspacePrivateLinkResourceGet.json
+# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2024-03-01/examples/privatelink/WorkspacePrivateLinkResourceGet.json
 if __name__ == "__main__":
     main()
