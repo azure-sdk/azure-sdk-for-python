b8f12ad625aa6859a019bf16567bdce970f50153
SDKAuto <sdkautomation@microsoft.com>
2024-02-29 00:18:34 +0000

    CodeGen from PR 27861 in Azure/azure-rest-api-specs Merge d6fcc46341f274b8af42a4cdcfa14e1f8d472619 into f428eaf0bbda62a0a5449ff16f7bb88a16411e33

--- sdk/purview/azure-mgmt-purview/generated_samples/private_link_resources_get_by_group_id.py
+++ sdk/purview/azure-mgmt-purview/generated_samples/private_link_resources_get_by_group_id.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/purview/resource-manager/Microsoft.Purview/stable/2021-07-01/examples/PrivateLinkResources_GetByGroupId.json
+# x-ms-original-file: specification/purview/resource-manager/Microsoft.Purview/preview/2023-05-01-preview/examples/PrivateLinkResources_GetByGroupId.json
 if __name__ == "__main__":
     main()
