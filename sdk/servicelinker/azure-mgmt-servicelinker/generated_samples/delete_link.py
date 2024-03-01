36874d617fc2550afb77e0e5ec6056f38b385287
SDKAuto <sdkautomation@microsoft.com>
2024-03-01 09:12:10 +0000

    CodeGen from PR 3215 in test-repo-billy/azure-rest-api-specs Merge 9f5cfb70eafe423359d2446fff464d523367102d into d8e7a994129b4ac979c55d3033220ac221241be0

--- sdk/servicelinker/azure-mgmt-servicelinker/generated_samples/delete_link.py
+++ sdk/servicelinker/azure-mgmt-servicelinker/generated_samples/delete_link.py
@@ -14,7 +14,7 @@ from azure.mgmt.servicelinker import ServiceLinkerManagementClient
     pip install azure-identity
     pip install azure-mgmt-servicelinker
 # USAGE
-    python operations_list.py
+    python delete_link.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -28,11 +28,12 @@ def main():
         credential=DefaultAzureCredential(),
     )
 
-    response = client.operations.list()
-    for item in response:
-        print(item)
+    client.linker.begin_delete(
+        resource_uri="subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app",
+        linker_name="linkName",
+    ).result()
 
 
-# x-ms-original-file: specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2023-05-01/examples/OperationsList.json
+# x-ms-original-file: specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2023-05-01/examples/DeleteLink.json
 if __name__ == "__main__":
     main()
