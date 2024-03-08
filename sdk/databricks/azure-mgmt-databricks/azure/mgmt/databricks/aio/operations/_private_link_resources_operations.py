020831cc158e6d3484502dfabd032a81c839530b
SDKAuto <sdkautomation@microsoft.com>
2024-03-08 00:37:39 +0000

    CodeGen from PR 28152 in Azure/azure-rest-api-specs Merge 5999d840031ad7a181e56e3fc289a63157c79802 into 0c16d1f7239f7c2c01c0cfd3b1c7b71855ef6661

--- sdk/databricks/azure-mgmt-databricks/azure/mgmt/databricks/aio/operations/_private_link_resources_operations.py
+++ sdk/databricks/azure-mgmt-databricks/azure/mgmt/databricks/aio/operations/_private_link_resources_operations.py
@@ -74,7 +74,7 @@ class PrivateLinkResourcesOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-02-01"))
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2024-05-01"))
         cls: ClsType[_models.PrivateLinkResourcesList] = kwargs.pop("cls", None)
 
         error_map = {
@@ -167,7 +167,7 @@ class PrivateLinkResourcesOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-02-01"))
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2024-05-01"))
         cls: ClsType[_models.GroupIdInformation] = kwargs.pop("cls", None)
 
         request = build_get_request(
