b32ceed94b80ecc34829463519bf6b2e22a1f459
SDKAuto <sdkautomation@microsoft.com>
2024-03-09 00:07:08 +0000

    CodeGen from PR 28001 in Azure/azure-rest-api-specs Merge f94b4f034b5902f29a2e9a2b97904a7c4ea75ece into 1b7ce7e606695c0207dbbf78c5a3b17508f3d194

--- sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/container_apps_create_or_update.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/container_apps_create_or_update.py
@@ -46,6 +46,10 @@ def main():
                         "logLevel": "debug",
                     },
                     "ingress": {
+                        "additionalPortMappings": [
+                            {"external": True, "targetPort": 1234},
+                            {"exposedPort": 3456, "external": False, "targetPort": 2345},
+                        ],
                         "clientCertificateMode": "accept",
                         "corsPolicy": {
                             "allowCredentials": True,
@@ -87,6 +91,7 @@ def main():
                         "traffic": [{"label": "production", "revisionName": "testcontainerApp0-ab1234", "weight": 100}],
                     },
                     "maxInactiveRevisions": 10,
+                    "runtime": {"dotnet": {"autoConfigureDataProtection": True}, "java": {"enableMetrics": True}},
                     "service": {"type": "redis"},
                 },
                 "environmentId": "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rg/providers/Microsoft.App/managedEnvironments/demokube",
@@ -106,6 +111,10 @@ def main():
                                     "periodSeconds": 3,
                                     "type": "Liveness",
                                 }
+                            ],
+                            "volumeMounts": [
+                                {"mountPath": "/mnt/path1", "subPath": "subPath1", "volumeName": "azurefile"},
+                                {"mountPath": "/mnt/path2", "subPath": "subPath2", "volumeName": "nfsazurefile"},
                             ],
                         }
                     ],
@@ -130,9 +139,15 @@ def main():
                     },
                     "serviceBinds": [
                         {
+                            "clientType": "dotnet",
+                            "customizedKeys": {"DesiredKey": "defaultKey"},
                             "name": "redisService",
                             "serviceId": "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rg/providers/Microsoft.App/containerApps/redisService",
                         }
+                    ],
+                    "volumes": [
+                        {"name": "azurefile", "storageName": "storage", "storageType": "AzureFile"},
+                        {"name": "nfsazurefile", "storageName": "nfsStorage", "storageType": "NfsAzureFile"},
                     ],
                 },
                 "workloadProfileName": "My-GP-01",
@@ -142,6 +157,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2023-05-01/examples/ContainerApps_CreateOrUpdate.json
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2024-02-02-preview/examples/ContainerApps_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
