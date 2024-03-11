ed74cdd5ff806d206b1b84ef0d81cd8726b2011b
SDKAuto <sdkautomation@microsoft.com>
2024-03-11 05:30:17 +0000

    CodeGen from PR 28125 in Azure/azure-rest-api-specs Merge d3fbc6db13ccc9118e8e9b456a80306cb127c9f5 into 41d1c179dc166b7f16b7e40ef4f2a1d9c85b10cc

--- sdk/security/azure-mgmt-security/azure/mgmt/security/models.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/models.py
@@ -23,8 +23,11 @@ from .v2022_05_01.models import *
 from .v2022_07_01_preview.models import *
 from .v2022_11_20_preview.models import *
 from .v2022_12_01_preview.models import *
-from .v2023_01_01.models import *
 from .v2023_01_01_preview.models import *
 from .v2023_02_01_preview.models import *
-from .v2023_03_01_preview.models import *
+from .v2023_05_01.models import *
+from .v2023_05_01_preview.models import *
+from .v2023_09_01_preview.models import *
+from .v2023_10_01_preview.models import *
 from .v2023_11_15.models import *
+from .v2024_01_01.models import *
