58f2a888bf30adbd5b33cdc7d93e2894f6c80794
SDKAuto <sdkautomation@microsoft.com>
2024-03-12 19:48:23 +0000

    CodeGen from PR 28228 in Azure/azure-rest-api-specs Merge dd44269651601043d61287c4b291ab1e5c0cf65e into db28b10bf402d6714f8d5b510b0453eadee74d73

--- sdk/azurelargeinstance/azure-mgmt-azurelargeinstance/azure/mgmt/azurelargeinstance/operations/_azure_large_instance_operations.py
+++ sdk/azurelargeinstance/azure-mgmt-azurelargeinstance/azure/mgmt/azurelargeinstance/operations/_azure_large_instance_operations.py
@@ -745,8 +745,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
@@ -968,8 +968,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
@@ -1099,8 +1099,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
