b68eb22c6b0425a11806478d47f4bedaf5071f93
SDKAuto <sdkautomation@microsoft.com>
2024-03-12 19:48:14 +0000

    CodeGen from PR 28228 in Azure/azure-rest-api-specs Merge dd44269651601043d61287c4b291ab1e5c0cf65e into db28b10bf402d6714f8d5b510b0453eadee74d73

--- sdk/azurelargeinstance/azure-mgmt-azurelargeinstance/azure/mgmt/azurelargeinstance/aio/operations/_azure_large_instance_operations.py
+++ sdk/azurelargeinstance/azure-mgmt-azurelargeinstance/azure/mgmt/azurelargeinstance/aio/operations/_azure_large_instance_operations.py
@@ -518,8 +518,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
@@ -741,8 +741,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
@@ -872,8 +872,8 @@ class AzureLargeInstanceOperations:
             deserialized = self._deserialize("OperationStatusResult", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
--- sdk/networkanalytics/azure-mgmt-networkanalytics/azure/mgmt/networkanalytics/operations/_data_products_operations.py
+++ sdk/networkanalytics/azure-mgmt-networkanalytics/azure/mgmt/networkanalytics/operations/_data_products_operations.py
@@ -975,8 +975,8 @@ class DataProductsOperations:
             deserialized = self._deserialize("DataProduct", pipeline_response)
 
         if response.status_code == 202:
-            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
             response_headers["Location"] = self._deserialize("str", response.headers.get("Location"))
+            response_headers["Retry-After"] = self._deserialize("int", response.headers.get("Retry-After"))
 
         if cls:
             return cls(pipeline_response, deserialized, response_headers)
