0da128ba3bb2411d98e3831f70e976cf06b3b897
SDKAuto <sdkautomation@microsoft.com>
2024-03-15 21:14:16 +0000

    CodeGen from PR 28287 in Azure/azure-rest-api-specs Merge f4f163b13aa1a27a205e82eb53e92ae6c417bbb6 into a7f8fb628797436ba442c2b59afb082a4080a9cd

--- sdk/developer-signing/azure-developer-signing/azure/developer/signing/models/__init__.py
+++ sdk/developer-signing/azure-developer-signing/azure/developer/signing/models/__init__.py
@@ -6,14 +6,20 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._operations import TextTranslationClientOperationsMixin
+from ._models import ExtendedKeyUsage
+from ._models import SignResult
+from ._models import SigningPayloadOptions
 
+from ._enums import SignatureAlgorithm
 from ._patch import __all__ as _patch_all
 from ._patch import *  # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "TextTranslationClientOperationsMixin",
+    "ExtendedKeyUsage",
+    "SignResult",
+    "SigningPayloadOptions",
+    "SignatureAlgorithm",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
