fc30d5857488c4bb8fe569849dfca83ecac5c001
SDKAuto <sdkautomation@microsoft.com>
2024-04-04 21:32:51 +0000

    CodeGen from PR 27738 in Azure/azure-rest-api-specs Merge da84ac1f0b41eefc50748630a4b58f340bffba89 into f304b25e052cb12600b417219cd6cfc664b8bcc2

--- sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alert_processing_rules_list_subscription.py
+++ sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alert_processing_rules_list_subscription.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.alertsmanagement import AlertsManagementClient
 
 """
@@ -34,6 +35,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/stable/2021-08-08/examples/AlertProcessingRules_List_Subscription.json
+# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2024-03-01-preview/examples/AlertProcessingRules_List_Subscription.json
 if __name__ == "__main__":
     main()
