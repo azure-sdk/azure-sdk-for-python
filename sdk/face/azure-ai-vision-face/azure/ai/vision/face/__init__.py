a6e2fa8a88bdf1b7f7653fcc316deb854a3b3fe9
SDKAuto <sdkautomation@microsoft.com>
2024-05-16 16:05:32 +0000

    CodeGen from PR 29123 in Azure/azure-rest-api-specs Merge 20919735ab0ced96b3676d212e99c88d4263a161 into 6e1615efb228e3aa8542af6dfc67bddb448e362d

--- sdk/face/azure-ai-vision-face/azure/ai/vision/face/__init__.py
+++ sdk/face/azure-ai-vision-face/azure/ai/vision/face/__init__.py
@@ -6,14 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._patch import FaceClient
+from ._client import FaceClient
 from ._client import FaceAdministrationClient
-from ._patch import FaceSessionClient
+from ._client import FaceSessionClient
 from ._version import VERSION
 
 __version__ = VERSION
 
-
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
@@ -21,6 +25,6 @@ __all__ = [
     "FaceAdministrationClient",
     "FaceSessionClient",
 ]
-
+__all__.extend([p for p in _patch_all if p not in __all__])
 
 _patch_sdk()
