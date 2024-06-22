959fa3805fd1a9de7f9f052f25d2356de8af6a84
SDKAuto <sdkautomation@microsoft.com>
2024-06-22 04:17:49 +0000

    CodeGen from PR 29549 in Azure/azure-rest-api-specs Merge d30746966c34f53c7d6a748ef20b84795ba29f87 into a83c34348fbd25ad79a05e36816b91da0122b583

--- sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/v2024_04_01/operations/_fleet_update_strategies_operations.py
+++ sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/v2024_04_01/operations/_fleet_update_strategies_operations.py
@@ -7,7 +7,8 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 from io import IOBase
-from typing import Any, Callable, Dict, IO, Iterable, Optional, TypeVar, Union, cast, overload
+import sys
+from typing import Any, Callable, Dict, IO, Iterable, Optional, Type, TypeVar, Union, cast, overload
 import urllib.parse
 
 from azure.core.exceptions import (
@@ -32,6 +33,10 @@ from .. import models as _models
 from ..._serialization import Serializer
 from .._vendor import _convert_request
 
+if sys.version_info >= (3, 9):
+    from collections.abc import MutableMapping
+else:
+    from typing import MutableMapping  # type: ignore  # pylint: disable=ungrouped-imports
 T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
 
@@ -267,7 +272,7 @@ class FleetUpdateStrategiesOperations:
         api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._api_version or "2024-04-01"))
         cls: ClsType[_models.FleetUpdateStrategyListResult] = kwargs.pop("cls", None)
 
-        error_map = {
+        error_map: MutableMapping[int, Type[HttpResponseError]] = {
             401: ClientAuthenticationError,
             404: ResourceNotFoundError,
             409: ResourceExistsError,
@@ -349,7 +354,7 @@ class FleetUpdateStrategiesOperations:
         :rtype: ~azure.mgmt.containerservicefleet.v2024_04_01.models.FleetUpdateStrategy
         :raises ~azure.core.exceptions.HttpResponseError:
         """
-        error_map = {
+        error_map: MutableMapping[int, Type[HttpResponseError]] = {
             401: ClientAuthenticationError,
             404: ResourceNotFoundError,
             409: ResourceExistsError,
@@ -404,7 +409,7 @@ class FleetUpdateStrategiesOperations:
         if_none_match: Optional[str] = None,
         **kwargs: Any
     ) -> _models.FleetUpdateStrategy:
-        error_map = {
+        error_map: MutableMapping[int, Type[HttpResponseError]] = {
             401: ClientAuthenticationError,
             404: ResourceNotFoundError,
             409: ResourceExistsError,
@@ -645,7 +650,7 @@ class FleetUpdateStrategiesOperations:
         if_match: Optional[str] = None,
         **kwargs: Any
     ) -> None:
-        error_map = {
+        error_map: MutableMapping[int, Type[HttpResponseError]] = {
             401: ClientAuthenticationError,
             404: ResourceNotFoundError,
             409: ResourceExistsError,
