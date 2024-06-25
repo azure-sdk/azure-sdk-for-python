c33b7e0cb2e4e2ebd1c0dbceea90ee6b4a867530
SDKAuto <sdkautomation@microsoft.com>
2024-06-25 11:55:07 +0000

    CodeGen from PR 28741 in Azure/azure-rest-api-specs Merge 4eb8e71d7678375acfb181fd286bf458c2740599 into 3aef2f96202eec656fca4abc4df0fbfe10b89ac0

--- sdk/botservice/azure-mgmt-botservice/generated_samples/delete_channel.py
+++ sdk/botservice/azure-mgmt-botservice/generated_samples/delete_channel.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.botservice import AzureBotService
 
 """
@@ -29,14 +30,13 @@ def main():
         subscription_id="subscription-id",
     )
 
-    response = client.channels.delete(
+    client.channels.delete(
         resource_group_name="OneResourceGroupName",
         resource_name="samplebotname",
         channel_name="EmailChannel",
     )
-    print(response)
 
 
-# x-ms-original-file: specification/botservice/resource-manager/Microsoft.BotService/stable/2022-09-15/examples/DeleteChannel.json
+# x-ms-original-file: specification/botservice/resource-manager/Microsoft.BotService/preview/2023-09-15-preview/examples/DeleteChannel.json
 if __name__ == "__main__":
     main()
