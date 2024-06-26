9850b6aae31e0d298da6180cfba5df271fc1852f
SDKAuto <sdkautomation@microsoft.com>
2024-06-26 13:43:03 +0000

    CodeGen from PR 29593 in Azure/azure-rest-api-specs Merge 6659df139a70f196cddb98acf2a349a8a8e6ad96 into d1296700aa6cd650970e9891dd58eef5698327fd

--- sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/mysql_flexibleservers/operations/_check_name_availability_without_location_operations.py
+++ sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/mysql_flexibleservers/operations/_check_name_availability_without_location_operations.py
@@ -44,7 +44,7 @@ def build_execute_request(subscription_id: str, **kwargs: Any) -> HttpRequest:
     _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
     _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-    api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-12-01-preview"))
+    api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-12-30"))
     content_type: Optional[str] = kwargs.pop("content_type", _headers.pop("Content-Type", None))
     accept = _headers.pop("Accept", "application/json")
 
@@ -152,7 +152,7 @@ class CheckNameAvailabilityWithoutLocationOperations:  # pylint: disable=name-to
         _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-12-01-preview"))
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2023-12-30"))
         content_type: Optional[str] = kwargs.pop("content_type", _headers.pop("Content-Type", None))
         cls: ClsType[_models.NameAvailability] = kwargs.pop("cls", None)
 
