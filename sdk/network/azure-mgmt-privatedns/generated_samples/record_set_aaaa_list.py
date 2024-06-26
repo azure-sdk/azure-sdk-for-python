a4ef4ef77f107775674f0e032b1f4b067ab744e9
SDKAuto <sdkautomation@microsoft.com>
2024-06-26 00:08:54 +0000

    CodeGen from PR 29470 in Azure/azure-rest-api-specs Merge 7fedc663c3f20e267db3f1c4e6376e58249e0cc6 into a220053360f9700f81abcdb6142769c85c3bcb27

--- sdk/network/azure-mgmt-privatedns/generated_samples/record_set_aaaa_list.py
+++ sdk/network/azure-mgmt-privatedns/generated_samples/record_set_aaaa_list.py
@@ -6,9 +6,15 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
+from typing import TYPE_CHECKING, Union
+
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.privatedns import PrivateDnsManagementClient
 
+if TYPE_CHECKING:
+    # pylint: disable=unused-import,ungrouped-imports
+    from .. import models as _models
 """
 # PREREQUISITES
     pip install azure-identity
@@ -38,6 +44,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2020-06-01/examples/RecordSetAAAAList.json
+# x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2024-06-01/examples/RecordSetAAAAList.json
 if __name__ == "__main__":
     main()
