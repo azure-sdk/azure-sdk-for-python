3fc756a93ff010a9232224edecd410f233227b56
SDKAuto <sdkautomation@microsoft.com>
2024-06-26 02:27:23 +0000

    CodeGen from PR 29586 in Azure/azure-rest-api-specs Merge 2dbe9eba8c360898a03835fa37a27eb066f90c9f into a220053360f9700f81abcdb6142769c85c3bcb27

--- sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/generated_samples/get_clusters_by_subscription_example.py
+++ sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/generated_samples/get_clusters_by_subscription_example.py
@@ -15,7 +15,7 @@ from azure.mgmt.hybridkubernetes import ConnectedKubernetesClient
     pip install azure-identity
     pip install azure-mgmt-hybridkubernetes
 # USAGE
-    python list_connected_cluster_operations_example.py
+    python get_clusters_by_subscription_example.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -27,14 +27,14 @@ from azure.mgmt.hybridkubernetes import ConnectedKubernetesClient
 def main():
     client = ConnectedKubernetesClient(
         credential=DefaultAzureCredential(),
-        subscription_id="SUBSCRIPTION_ID",
+        subscription_id="1bfbb5d0-917e-4346-9026-1d3b344417f5",
     )
 
-    response = client.operations.get()
+    response = client.connected_cluster.list_by_subscription()
     for item in response:
         print(item)
 
 
-# x-ms-original-file: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2024-06-01-preview/examples/ListConnectedClusterOperationsExample.json
+# x-ms-original-file: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2024-06-01-preview/examples/GetClustersBySubscriptionExample.json
 if __name__ == "__main__":
     main()
--- sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/generated_samples/get_provisioned_cluster_example.py
+++ sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/generated_samples/get_provisioned_cluster_example.py
@@ -15,7 +15,7 @@ from azure.mgmt.hybridkubernetes import ConnectedKubernetesClient
     pip install azure-identity
     pip install azure-mgmt-hybridkubernetes
 # USAGE
-    python list_connected_cluster_operations_example.py
+    python get_provisioned_cluster_example.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -27,14 +27,16 @@ from azure.mgmt.hybridkubernetes import ConnectedKubernetesClient
 def main():
     client = ConnectedKubernetesClient(
         credential=DefaultAzureCredential(),
-        subscription_id="SUBSCRIPTION_ID",
+        subscription_id="1bfbb5d0-917e-4346-9026-1d3b344417f5",
     )
 
-    response = client.operations.get()
-    for item in response:
-        print(item)
+    response = client.connected_cluster.get(
+        resource_group_name="k8sc-rg",
+        cluster_name="testCluster",
+    )
+    print(response)
 
 
-# x-ms-original-file: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2024-06-01-preview/examples/ListConnectedClusterOperationsExample.json
+# x-ms-original-file: specification/hybridkubernetes/resource-manager/Microsoft.Kubernetes/preview/2024-06-01-preview/examples/GetProvisionedClusterExample.json
 if __name__ == "__main__":
     main()
