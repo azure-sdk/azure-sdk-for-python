bb38f57d38c414b5d5626adc758695acd3b3c706
SDKAuto <sdkautomation@microsoft.com>
2024-08-07 10:51:04 +0000

    CodeGen from PR 29330 in Azure/azure-rest-api-specs Merge 8d22b7e5cc45dcad545f00a1115d396758f69d0a into c1d839d48ee936c9338431c38f2cbbfbc9879ea2

--- sdk/mongocluster/azure-mgmt-mongocluster/azure/mgmt/mongocluster/_client.py
+++ sdk/mongocluster/azure-mgmt-mongocluster/azure/mgmt/mongocluster/_client.py
@@ -23,6 +23,7 @@ from .operations import (
     Operations,
     PrivateEndpointConnectionsOperations,
     PrivateLinksOperations,
+    ReplicasOperations,
 )
 
 if TYPE_CHECKING:
@@ -45,6 +46,8 @@ class MongoClusterMgmtClient:  # pylint: disable=client-accepts-api-version-keyw
      azure.mgmt.mongocluster.operations.PrivateEndpointConnectionsOperations
     :ivar private_links: PrivateLinksOperations operations
     :vartype private_links: azure.mgmt.mongocluster.operations.PrivateLinksOperations
+    :ivar replicas: ReplicasOperations operations
+    :vartype replicas: azure.mgmt.mongocluster.operations.ReplicasOperations
     :param credential: Credential used to authenticate requests to the service. Required.
     :type credential: ~azure.core.credentials.TokenCredential
     :param subscription_id: The ID of the target subscription. The value must be an UUID. Required.
@@ -52,7 +55,7 @@ class MongoClusterMgmtClient:  # pylint: disable=client-accepts-api-version-keyw
     :param base_url: Service host. Default value is "https://management.azure.com".
     :type base_url: str
     :keyword api_version: The API version to use for this operation. Default value is
-     "2024-03-01-preview". Note that overriding this default value may result in unsupported
+     "2024-06-01-preview". Note that overriding this default value may result in unsupported
      behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
@@ -99,6 +102,7 @@ class MongoClusterMgmtClient:  # pylint: disable=client-accepts-api-version-keyw
             self._client, self._config, self._serialize, self._deserialize
         )
         self.private_links = PrivateLinksOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.replicas = ReplicasOperations(self._client, self._config, self._serialize, self._deserialize)
 
     def send_request(self, request: HttpRequest, *, stream: bool = False, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
