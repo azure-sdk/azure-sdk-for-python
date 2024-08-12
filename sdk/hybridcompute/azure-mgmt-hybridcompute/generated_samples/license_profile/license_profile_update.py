82caa7d0999e5a06a1af20d65612668eec9dbb84
SDKAuto <sdkautomation@microsoft.com>
2024-08-12 06:39:42 +0000

    CodeGen from PR 29954 in Azure/azure-rest-api-specs Merge f6fda53ee0e945e829cc4b8e4db11e7fe39f8e30 into cac34a4eb170a1fb08635573362a8923f106421d

--- sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/license_profile/license_profile_update.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/license_profile/license_profile_update.py
@@ -6,6 +6,8 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
+from typing import Any, IO, Union
+
 from azure.identity import DefaultAzureCredential
 
 from azure.mgmt.hybridcompute import HybridComputeManagementClient
@@ -15,7 +17,7 @@ from azure.mgmt.hybridcompute import HybridComputeManagementClient
     pip install azure-identity
     pip install azure-mgmt-hybridcompute
 # USAGE
-    python gateway_delete.py
+    python license_profile_update.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -27,15 +29,27 @@ from azure.mgmt.hybridcompute import HybridComputeManagementClient
 def main():
     client = HybridComputeManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id="ffd506c8-3415-42d3-9612-fdb423fb17df",
+        subscription_id="{subscriptionId}",
     )
 
-    client.gateways.begin_delete(
+    response = client.license_profiles.begin_update(
         resource_group_name="myResourceGroup",
-        gateway_name="{gatewayName}",
+        machine_name="myMachine",
+        parameters={
+            "properties": {
+                "esuProfile": {"assignedLicense": "{LicenseResourceId}"},
+                "productProfile": {
+                    "productFeatures": [{"name": "Hotpatch", "subscriptionStatus": "Enable"}],
+                    "productType": "WindowsServer",
+                    "subscriptionStatus": "Enable",
+                },
+                "softwareAssurance": {"softwareAssuranceCustomer": True},
+            }
+        },
     ).result()
+    print(response)
 
 
-# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-05-20-preview/examples/gateway/Gateway_Delete.json
+# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/stable/2024-07-10/examples/licenseProfile/LicenseProfile_Update.json
 if __name__ == "__main__":
     main()
