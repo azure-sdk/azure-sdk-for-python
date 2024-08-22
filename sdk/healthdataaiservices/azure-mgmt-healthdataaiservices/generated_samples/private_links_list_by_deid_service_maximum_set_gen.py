ac2dab116988b14bec7a58d8f05db322c72a1140
SDKAuto <sdkautomation@microsoft.com>
2024-08-22 09:14:15 +0000

    CodeGen from PR 3487 in test-repo-billy/azure-rest-api-specs Merge 204e18c04d9fe050100729e19cffb7c95b8303ad into c221294c903b4bcbe7b1ee4bce901322a7056943

--- sdk/healthdataaiservices/azure-mgmt-healthdataaiservices/generated_samples/private_links_list_by_deid_service_maximum_set_gen.py
+++ sdk/healthdataaiservices/azure-mgmt-healthdataaiservices/generated_samples/private_links_list_by_deid_service_maximum_set_gen.py
@@ -15,7 +15,7 @@ from azure.mgmt.healthdataaiservices import HealthDataAIServicesMgmtClient
     pip install azure-identity
     pip install azure-mgmt-healthdataaiservices
 # USAGE
-    python deid_services_list_by_resource_group_maximum_set_gen.py
+    python private_links_list_by_deid_service_maximum_set_gen.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -27,11 +27,12 @@ from azure.mgmt.healthdataaiservices import HealthDataAIServicesMgmtClient
 def main():
     client = HealthDataAIServicesMgmtClient(
         credential=DefaultAzureCredential(),
-        subscription_id="F21BB31B-C214-42C0-ACF0-DACCA05D3011",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.deid_services.list_by_resource_group(
+    response = client.private_links.list_by_deid_service(
         resource_group_name="rgopenapi",
+        deid_service_name="deidTest",
     )
     for item in response:
         print(item)
