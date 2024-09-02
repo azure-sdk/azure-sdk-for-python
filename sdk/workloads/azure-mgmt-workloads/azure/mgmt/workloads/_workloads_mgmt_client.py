179d8f7d28525090dfd2c723d49530d2126158dc
SDKAuto <sdkautomation@microsoft.com>
2024-09-02 02:23:45 +0000

    CodeGen from PR 3490 in test-repo-billy/azure-rest-api-specs Merge 603f2404beffcfb01517b318ba2a1b19bc271169 into 2b91022c223ed3618c268c750f831b76fe0f4fb5

--- sdk/workloads/azure-mgmt-workloads/azure/mgmt/workloads/_workloads_mgmt_client.py
+++ sdk/workloads/azure-mgmt-workloads/azure/mgmt/workloads/_workloads_mgmt_client.py
@@ -8,9 +8,12 @@
 
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
+from typing_extensions import Self
 
+from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core.policies import ARMAutoResourceProviderRegistrationPolicy
 
 from . import models as _models
 from ._configuration import WorkloadsMgmtClientConfiguration
@@ -35,10 +38,7 @@ if TYPE_CHECKING:
 class WorkloadsMgmtClient(
     WorkloadsMgmtClientOperationsMixin
 ):  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
-    """Workloads client provides access to various workload operations.:code:`<br>`Azure Center for
-    SAP solutions is currently in PREVIEW. See the `Azure Center for SAP solutions - Legal Terms
-    <https://learn.microsoft.com/en-us/legal/azure-center-for-sap-solutions/azure-center-for-sap-solutions-legal-terms>`_
-    for legal notices applicable to Azure Center for SAP solutions.
+    """Workloads client provides access to various workload operations.
 
     :ivar sap_virtual_instances: SAPVirtualInstancesOperations operations
     :vartype sap_virtual_instances: azure.mgmt.workloads.operations.SAPVirtualInstancesOperations
@@ -80,7 +80,25 @@ class WorkloadsMgmtClient(
         self._config = WorkloadsMgmtClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                ARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -107,7 +125,7 @@ class WorkloadsMgmtClient(
         )
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, *, stream: bool = False, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -127,12 +145,12 @@ class WorkloadsMgmtClient(
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     def close(self) -> None:
         self._client.close()
 
-    def __enter__(self) -> "WorkloadsMgmtClient":
+    def __enter__(self) -> Self:
         self._client.__enter__()
         return self
 
