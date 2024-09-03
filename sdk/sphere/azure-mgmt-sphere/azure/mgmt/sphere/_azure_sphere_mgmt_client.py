60a70e53cfbbc02bd7ea215df453b8b08369d1c4
SDKAuto <sdkautomation@microsoft.com>
2024-09-03 17:10:38 +0000

    CodeGen from PR 30384 in Azure/azure-rest-api-specs Merge d38dec7f7254d65a9f9a3a8b079eedf86db06361 into d9bdb6a00e47f00da22932ba700db365ece10b29

--- sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/_azure_sphere_mgmt_client.py
+++ sdk/sphere/azure-mgmt-sphere/azure/mgmt/sphere/_azure_sphere_mgmt_client.py
@@ -8,6 +8,7 @@
 
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
+from typing_extensions import Self
 
 from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
@@ -133,7 +134,7 @@ class AzureSphereMgmtClient:  # pylint: disable=client-accepts-api-version-keywo
     def close(self) -> None:
         self._client.close()
 
-    def __enter__(self) -> "AzureSphereMgmtClient":
+    def __enter__(self) -> Self:
         self._client.__enter__()
         return self
 
