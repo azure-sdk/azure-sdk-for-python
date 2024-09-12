ff13d44666507ea2442e28e9fb8bd65bc23f2d87
SDKAuto <sdkautomation@microsoft.com>
2024-09-12 09:02:40 +0000

    CodeGen from PR 30235 in Azure/azure-rest-api-specs Merge 9da0f6e77b5b039ec543c6290fcddcda0f73e955 into 4e5b64a647cccbddbfac5c4a9635f751def8feb0

--- sdk/security/azure-mgmt-security/azure/mgmt/security/v2021_06_01/aio/operations/_assessments_metadata_operations.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/v2021_06_01/aio/operations/_assessments_metadata_operations.py
@@ -21,15 +21,13 @@ from azure.core.exceptions import (
     map_error,
 )
 from azure.core.pipeline import PipelineResponse
-from azure.core.pipeline.transport import AsyncHttpResponse
-from azure.core.rest import HttpRequest
+from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.core.tracing.decorator import distributed_trace
 from azure.core.tracing.decorator_async import distributed_trace_async
 from azure.core.utils import case_insensitive_dict
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
 from ... import models as _models
-from ..._vendor import _convert_request
 from ...operations._assessments_metadata_operations import (
     build_create_in_subscription_request,
     build_delete_in_subscription_request,
@@ -99,7 +97,6 @@ class AssessmentsMetadataOperations:
                     headers=_headers,
                     params=_params,
                 )
-                _request = _convert_request(_request)
                 _request.url = self._client.format_url(_request.url)
 
             else:
@@ -115,7 +112,6 @@ class AssessmentsMetadataOperations:
                 _request = HttpRequest(
                     "GET", urllib.parse.urljoin(next_link, _parsed_next_link.path), params=_next_request_params
                 )
-                _request = _convert_request(_request)
                 _request.url = self._client.format_url(_request.url)
                 _request.method = "GET"
             return _request
@@ -175,7 +171,6 @@ class AssessmentsMetadataOperations:
             headers=_headers,
             params=_params,
         )
-        _request = _convert_request(_request)
         _request.url = self._client.format_url(_request.url)
 
         _stream = False
@@ -189,7 +184,7 @@ class AssessmentsMetadataOperations:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             raise HttpResponseError(response=response, error_format=ARMErrorFormat)
 
-        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response)
+        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response.http_response)
 
         if cls:
             return cls(pipeline_response, deserialized, {})  # type: ignore
@@ -229,7 +224,6 @@ class AssessmentsMetadataOperations:
                     headers=_headers,
                     params=_params,
                 )
-                _request = _convert_request(_request)
                 _request.url = self._client.format_url(_request.url)
 
             else:
@@ -245,7 +239,6 @@ class AssessmentsMetadataOperations:
                 _request = HttpRequest(
                     "GET", urllib.parse.urljoin(next_link, _parsed_next_link.path), params=_next_request_params
                 )
-                _request = _convert_request(_request)
                 _request.url = self._client.format_url(_request.url)
                 _request.method = "GET"
             return _request
@@ -308,7 +301,6 @@ class AssessmentsMetadataOperations:
             headers=_headers,
             params=_params,
         )
-        _request = _convert_request(_request)
         _request.url = self._client.format_url(_request.url)
 
         _stream = False
@@ -322,7 +314,7 @@ class AssessmentsMetadataOperations:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             raise HttpResponseError(response=response, error_format=ARMErrorFormat)
 
-        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response)
+        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response.http_response)
 
         if cls:
             return cls(pipeline_response, deserialized, {})  # type: ignore
@@ -431,7 +423,6 @@ class AssessmentsMetadataOperations:
             headers=_headers,
             params=_params,
         )
-        _request = _convert_request(_request)
         _request.url = self._client.format_url(_request.url)
 
         _stream = False
@@ -445,7 +436,7 @@ class AssessmentsMetadataOperations:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             raise HttpResponseError(response=response, error_format=ARMErrorFormat)
 
-        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response)
+        deserialized = self._deserialize("SecurityAssessmentMetadataResponse", pipeline_response.http_response)
 
         if cls:
             return cls(pipeline_response, deserialized, {})  # type: ignore
@@ -487,7 +478,6 @@ class AssessmentsMetadataOperations:
             headers=_headers,
             params=_params,
         )
-        _request = _convert_request(_request)
         _request.url = self._client.format_url(_request.url)
 
         _stream = False
