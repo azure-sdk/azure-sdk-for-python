80b47f16aaecf04d76d73d4b99b30f429dbd6079
SDKAuto <sdkautomation@microsoft.com>
2024-09-26 03:50:33 +0000

    CodeGen from PR 30401 in Azure/azure-rest-api-specs Merge 34f160bdcf442d3c59f3593a7de658d02a178350 into b478dcdabacb37945ff89daa0eda1ae1afc98db4

--- sdk/scvmm/azure-mgmt-scvmm/generated_samples/vmm_servers_create_or_update_maximum_set_gen.py
+++ sdk/scvmm/azure-mgmt-scvmm/generated_samples/vmm_servers_create_or_update_maximum_set_gen.py
@@ -6,8 +6,6 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Any, IO, Union
-
 from azure.identity import DefaultAzureCredential
 
 from azure.mgmt.scvmm import ScVmmMgmtClient
@@ -52,6 +50,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/VmmServers_CreateOrUpdate_MaximumSet_Gen.json
+# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2024-06-01/examples/VmmServers_CreateOrUpdate_MaximumSet_Gen.json
 if __name__ == "__main__":
     main()
