ffe241bb862d94aec0afa2414ddb01f00653d845
SDKAuto <sdkautomation@microsoft.com>
2024-09-27 20:18:09 +0000

    CodeGen from PR 30603 in Azure/azure-rest-api-specs Merge ae0132085b7736568d84b48fe8d347c14c4129da into ae95eb6a4701d844bada7d1c4f5ecf4a7444e5b8

--- sdk/iothub/azure-mgmt-iothubprovisioningservices/generated_samples/dps_delete.py
+++ sdk/iothub/azure-mgmt-iothubprovisioningservices/generated_samples/dps_delete.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.iothubprovisioningservices import IotDpsClient
 
 """
@@ -35,6 +36,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/preview/2023-03-01-preview/examples/DPSDelete.json
+# x-ms-original-file: specification/deviceprovisioningservices/resource-manager/Microsoft.Devices/preview/2025-02-01-preview/examples/DPSDelete.json
 if __name__ == "__main__":
     main()
