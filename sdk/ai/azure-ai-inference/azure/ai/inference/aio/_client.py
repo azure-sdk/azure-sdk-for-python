8ff1c6d9b960e2f5fb7f501bd2397b1dc3220697
SDKAuto <sdkautomation@microsoft.com>
2024-09-27 19:54:32 +0000

    CodeGen from PR 29763 in Azure/azure-rest-api-specs Merge aa97e50e44d2c620c92e5a4c791bb387a741d64c into 7507aafa42d29e3526d8e5aacf6233c0e9a959d0

--- sdk/ai/azure-ai-inference/azure/ai/inference/aio/_client.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/aio/_client.py
@@ -37,10 +37,9 @@ class ChatCompletionsClient(ChatCompletionsClientOperationsMixin):  # pylint: di
 
     :param endpoint: Service host. Required.
     :type endpoint: str
-    :param credential: Credential used to authenticate requests to the service. Is one of the
-     following types: AzureKeyCredential, AzureKeyCredential, TokenCredential Required.
+    :param credential: Credential used to authenticate requests to the service. Is either a
+     AzureKeyCredential type or a TokenCredential type. Required.
     :type credential: ~azure.core.credentials.AzureKeyCredential or
-     ~azure.core.credentials.AzureKeyCredential or
      ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: The API version to use for this operation. Default value is
      "2024-05-01-preview". Note that overriding this default value may result in unsupported
@@ -120,10 +119,9 @@ class EmbeddingsClient(EmbeddingsClientOperationsMixin):  # pylint: disable=clie
 
     :param endpoint: Service host. Required.
     :type endpoint: str
-    :param credential: Credential used to authenticate requests to the service. Is one of the
-     following types: AzureKeyCredential, AzureKeyCredential, TokenCredential Required.
+    :param credential: Credential used to authenticate requests to the service. Is either a
+     AzureKeyCredential type or a TokenCredential type. Required.
     :type credential: ~azure.core.credentials.AzureKeyCredential or
-     ~azure.core.credentials.AzureKeyCredential or
      ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: The API version to use for this operation. Default value is
      "2024-05-01-preview". Note that overriding this default value may result in unsupported
@@ -203,10 +201,9 @@ class ImageEmbeddingsClient(ImageEmbeddingsClientOperationsMixin):  # pylint: di
 
     :param endpoint: Service host. Required.
     :type endpoint: str
-    :param credential: Credential used to authenticate requests to the service. Is one of the
-     following types: AzureKeyCredential, AzureKeyCredential, TokenCredential Required.
+    :param credential: Credential used to authenticate requests to the service. Is either a
+     AzureKeyCredential type or a TokenCredential type. Required.
     :type credential: ~azure.core.credentials.AzureKeyCredential or
-     ~azure.core.credentials.AzureKeyCredential or
      ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: The API version to use for this operation. Default value is
      "2024-05-01-preview". Note that overriding this default value may result in unsupported
