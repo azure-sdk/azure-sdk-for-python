7686fdc7e7612f6c56c5b2ac640cd8922b3fc373
SDKAuto <sdkautomation@microsoft.com>
2024-10-10 16:18:28 +0000

    CodeGen from PR 30939 in Azure/azure-rest-api-specs Merge b6868535a2409753736e545d9b189a79029074d0 into 92468a26e4b3a77becc155dd56e2afb28615bf9b

--- sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/delete_management_group.py
+++ sdk/managementgroups/azure-mgmt-managementgroups/generated_samples/delete_management_group.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.managementgroups import ManagementGroupsAPI
 
 """
@@ -34,6 +35,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/DeleteManagementGroup.json
+# x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2023-04-01/examples/DeleteManagementGroup.json
 if __name__ == "__main__":
     main()
