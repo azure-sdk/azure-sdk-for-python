d2ec108c990b547a9e2e0c5ccfe956d47eaf3a34
SDKAuto <sdkautomation@microsoft.com>
2024-10-11 03:04:43 +0000

    CodeGen from PR 30477 in Azure/azure-rest-api-specs Merge 423cb0ccd719cae78313c29a669a47d8166afa16 into fda3d5e4ae4c69aa72ddfdf5ca1b6d5795a03e71

--- sdk/loganalytics/azure-mgmt-loganalytics/generated_samples/workspaces_list_management_groups.py
+++ sdk/loganalytics/azure-mgmt-loganalytics/generated_samples/workspaces_list_management_groups.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.loganalytics import LogAnalyticsManagementClient
 
 """
@@ -37,6 +38,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/stable/2020-08-01/examples/WorkspacesListManagementGroups.json
+# x-ms-original-file: specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/stable/2023-09-01/examples/WorkspacesListManagementGroups.json
 if __name__ == "__main__":
     main()
