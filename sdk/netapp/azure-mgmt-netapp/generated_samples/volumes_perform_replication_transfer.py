557ae80450bd302557049c6e4745a50bb16c2fb7
SDKAuto <sdkautomation@microsoft.com>
2024-10-14 14:33:56 +0000

    CodeGen from PR 30268 in Azure/azure-rest-api-specs Merge 7a18b2486ca98f4debda188ce11d1c5a9daea862 into 9ce76c4f113368361dac4de5d0a71eb058602756

--- sdk/netapp/azure-mgmt-netapp/generated_samples/volumes_perform_replication_transfer.py
+++ sdk/netapp/azure-mgmt-netapp/generated_samples/volumes_perform_replication_transfer.py
@@ -15,7 +15,7 @@ from azure.mgmt.netapp import NetAppManagementClient
     pip install azure-identity
     pip install azure-mgmt-netapp
 # USAGE
-    python backup_policies_list.py
+    python volumes_perform_replication_transfer.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -30,14 +30,14 @@ def main():
         subscription_id="D633CC2E-722B-4AE1-B636-BBD9E4C60ED9",
     )
 
-    response = client.backup_policies.list(
+    client.volumes.begin_perform_replication_transfer(
         resource_group_name="myRG",
         account_name="account1",
-    )
-    for item in response:
-        print(item)
+        pool_name="pool1",
+        volume_name="volume1",
+    ).result()
 
 
-# x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2024-05-01/examples/BackupPolicies_List.json
+# x-ms-original-file: specification/netapp/resource-manager/Microsoft.NetApp/stable/2024-05-01/examples/Volumes_PerformReplicationTransfer.json
 if __name__ == "__main__":
     main()
--- sdk/netapp/azure-mgmt-netapp/generated_tests/test_net_app_management_volume_groups_operations.py
+++ sdk/netapp/azure-mgmt-netapp/generated_tests/test_net_app_management_volume_groups_operations.py
@@ -24,7 +24,7 @@ class TestNetAppManagementVolumeGroupsOperations(AzureMgmtRecordedTestCase):
         response = self.client.volume_groups.list_by_net_app_account(
             resource_group_name=resource_group.name,
             account_name="str",
-            api_version="2024-03-01",
+            api_version="2024-05-01",
         )
         result = [r for r in response]
         # please add some check logic here by yourself
@@ -37,7 +37,7 @@ class TestNetAppManagementVolumeGroupsOperations(AzureMgmtRecordedTestCase):
             resource_group_name=resource_group.name,
             account_name="str",
             volume_group_name="str",
-            api_version="2024-03-01",
+            api_version="2024-05-01",
         )
 
         # please add some check logic here by yourself
@@ -82,6 +82,7 @@ class TestNetAppManagementVolumeGroupsOperations(AzureMgmtRecordedTestCase):
                             "replication": {
                                 "remoteVolumeResourceId": "str",
                                 "endpointType": "str",
+                                "remotePath": {"externalHostName": "str", "serverName": "str", "volumeName": "str"},
                                 "remoteVolumeRegion": "str",
                                 "replicationId": "str",
                                 "replicationSchedule": "str",
@@ -160,7 +161,7 @@ class TestNetAppManagementVolumeGroupsOperations(AzureMgmtRecordedTestCase):
                     }
                 ],
             },
-            api_version="2024-03-01",
+            api_version="2024-05-01",
         ).result()  # call '.result()' to poll until service return final result
 
         # please add some check logic here by yourself
@@ -173,7 +174,7 @@ class TestNetAppManagementVolumeGroupsOperations(AzureMgmtRecordedTestCase):
             resource_group_name=resource_group.name,
             account_name="str",
             volume_group_name="str",
-            api_version="2024-03-01",
+            api_version="2024-05-01",
         ).result()  # call '.result()' to poll until service return final result
 
         # please add some check logic here by yourself
