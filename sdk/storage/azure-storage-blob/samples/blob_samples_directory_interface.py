648784c59e36188f47baee91350b4ea220a07a27
SDKAuto <sdkautomation@microsoft.com>
2024-10-18 21:11:58 +0000

    CodeGen from PR 31103 in Azure/azure-rest-api-specs Merge 6b9b4cc3bc2ce00b5ac0a0e3fbca4659257a7daf into cfe318beba82c1e5bda6a600f91de32cba814364

--- sdk/storage/azure-storage-blob/samples/blob_samples_directory_interface.py
+++ sdk/storage/azure-storage-blob/samples/blob_samples_directory_interface.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-'''
+"""
 FILE: blob_samples_directory_interface.py
 DESCRIPTION:
     This example shows how to perform common filesystem-like operations on a
@@ -23,180 +23,179 @@ DESCRIPTION:
   USAGE: python blob_samples_directory_interface.py CONTAINER_NAME
     Set the environment variables with your own values before running the sample:
     1) AZURE_STORAGE_CONNECTION_STRING - the connection string to your storage account
-'''
+"""
 
 import os
 from azure.storage.blob import BlobServiceClient
 
+
 class DirectoryClient:
-  def __init__(self, connection_string, container_name):
-    service_client = BlobServiceClient.from_connection_string(connection_string)
-    self.client = service_client.get_container_client(container_name)
+    def __init__(self, connection_string, container_name):
+        service_client = BlobServiceClient.from_connection_string(connection_string)
+        self.client = service_client.get_container_client(container_name)
 
-  def upload(self, source, dest):
-    '''
-    Upload a file or directory to a path inside the container
-    '''
-    if (os.path.isdir(source)):
-      self.upload_dir(source, dest)
-    else:
-      self.upload_file(source, dest)
+    def upload(self, source, dest):
+        """
+        Upload a file or directory to a path inside the container
+        """
+        if os.path.isdir(source):
+            self.upload_dir(source, dest)
+        else:
+            self.upload_file(source, dest)
 
-  def upload_file(self, source, dest):
-    '''
-    Upload a single file to a path inside the container
-    '''
-    print(f'Uploading {source} to {dest}')
-    with open(source, 'rb') as data:
-      self.client.upload_blob(name=dest, data=data)
+    def upload_file(self, source, dest):
+        """
+        Upload a single file to a path inside the container
+        """
+        print(f"Uploading {source} to {dest}")
+        with open(source, "rb") as data:
+            self.client.upload_blob(name=dest, data=data)
 
-  def upload_dir(self, source, dest):
-    '''
-    Upload a directory to a path inside the container
-    '''
-    prefix = '' if dest == '' else dest + '/'
-    prefix += os.path.basename(source) + '/'
-    for root, dirs, files in os.walk(source):
-      for name in files:
-        dir_part = os.path.relpath(root, source)
-        dir_part = '' if dir_part == '.' else dir_part + '/'
-        file_path = os.path.join(root, name)
-        blob_path = prefix + dir_part + name
-        self.upload_file(file_path, blob_path)
+    def upload_dir(self, source, dest):
+        """
+        Upload a directory to a path inside the container
+        """
+        prefix = "" if dest == "" else dest + "/"
+        prefix += os.path.basename(source) + "/"
+        for root, dirs, files in os.walk(source):
+            for name in files:
+                dir_part = os.path.relpath(root, source)
+                dir_part = "" if dir_part == "." else dir_part + "/"
+                file_path = os.path.join(root, name)
+                blob_path = prefix + dir_part + name
+                self.upload_file(file_path, blob_path)
 
-  def download(self, source, dest):
-    '''
-    Download a file or directory to a path on the local filesystem
-    '''
-    if not dest:
-      raise Exception('A destination must be provided')
+    def download(self, source, dest):
+        """
+        Download a file or directory to a path on the local filesystem
+        """
+        if not dest:
+            raise Exception("A destination must be provided")
 
-    blobs = self.ls_files(source, recursive=True)
-    if blobs:
-      # if source is a directory, dest must also be a directory
-      if not source == '' and not source.endswith('/'):
-        source += '/'
-      if not dest.endswith('/'):
-        dest += '/'
-      # append the directory name from source to the destination
-      dest += os.path.basename(os.path.normpath(source)) + '/'
+        blobs = self.ls_files(source, recursive=True)
+        if blobs:
+            # if source is a directory, dest must also be a directory
+            if not source == "" and not source.endswith("/"):
+                source += "/"
+            if not dest.endswith("/"):
+                dest += "/"
+            # append the directory name from source to the destination
+            dest += os.path.basename(os.path.normpath(source)) + "/"
 
-      blobs = [source + blob for blob in blobs]
-      for blob in blobs:
-        blob_dest = dest + os.path.relpath(blob, source)
-        self.download_file(blob, blob_dest)
-    else:
-      self.download_file(source, dest)
+            blobs = [source + blob for blob in blobs]
+            for blob in blobs:
+                blob_dest = dest + os.path.relpath(blob, source)
+                self.download_file(blob, blob_dest)
+        else:
+            self.download_file(source, dest)
 
-  def download_file(self, source, dest):
-    '''
-    Download a single file to a path on the local filesystem
-    '''
-    # dest is a directory if ending with '/' or '.', otherwise it's a file
-    if dest.endswith('.'):
-      dest += '/'
-    blob_dest = dest + os.path.basename(source) if dest.endswith('/') else dest
+    def download_file(self, source, dest):
+        """
+        Download a single file to a path on the local filesystem
+        """
+        # dest is a directory if ending with '/' or '.', otherwise it's a file
+        if dest.endswith("."):
+            dest += "/"
+        blob_dest = dest + os.path.basename(source) if dest.endswith("/") else dest
 
-    print(f'Downloading {source} to {blob_dest}')
-    os.makedirs(os.path.dirname(blob_dest), exist_ok=True)
-    bc = self.client.get_blob_client(blob=source)
-    if not dest.endswith('/'):
-        with open(blob_dest, 'wb') as file:
-          data = bc.download_blob()
-          file.write(data.readall())
+        print(f"Downloading {source} to {blob_dest}")
+        os.makedirs(os.path.dirname(blob_dest), exist_ok=True)
+        bc = self.client.get_blob_client(blob=source)
+        if not dest.endswith("/"):
+            with open(blob_dest, "wb") as file:
+                data = bc.download_blob()
+                file.write(data.readall())
 
-  def ls_files(self, path, recursive=False):
-    '''
-    List files under a path, optionally recursively
-    '''
-    if not path == '' and not path.endswith('/'):
-      path += '/'
+    def ls_files(self, path, recursive=False):
+        """
+        List files under a path, optionally recursively
+        """
+        if not path == "" and not path.endswith("/"):
+            path += "/"
 
-    blob_iter = self.client.list_blobs(name_starts_with=path)
-    files = []
-    for blob in blob_iter:
-      relative_path = os.path.relpath(blob.name, path)
-      if recursive or not '/' in relative_path:
-        files.append(relative_path)
-    return files
+        blob_iter = self.client.list_blobs(name_starts_with=path)
+        files = []
+        for blob in blob_iter:
+            relative_path = os.path.relpath(blob.name, path)
+            if recursive or not "/" in relative_path:
+                files.append(relative_path)
+        return files
 
-  def ls_dirs(self, path, recursive=False):
-    '''
-    List directories under a path, optionally recursively
-    '''
-    if not path == '' and not path.endswith('/'):
-      path += '/'
+    def ls_dirs(self, path, recursive=False):
+        """
+        List directories under a path, optionally recursively
+        """
+        if not path == "" and not path.endswith("/"):
+            path += "/"
 
-    blob_iter = self.client.list_blobs(name_starts_with=path)
-    dirs = []
-    for blob in blob_iter:
-      relative_dir = os.path.dirname(os.path.relpath(blob.name, path))
-      if relative_dir and (recursive or not '/' in relative_dir) and not relative_dir in dirs:
-        dirs.append(relative_dir)
+        blob_iter = self.client.list_blobs(name_starts_with=path)
+        dirs = []
+        for blob in blob_iter:
+            relative_dir = os.path.dirname(os.path.relpath(blob.name, path))
+            if relative_dir and (recursive or not "/" in relative_dir) and not relative_dir in dirs:
+                dirs.append(relative_dir)
 
-    return dirs
+        return dirs
 
-  def rm(self, path, recursive=False):
-    '''
-    Remove a single file, or remove a path recursively
-    '''
-    if recursive:
-      self.rmdir(path)
-    else:
-      print(f'Deleting {path}')
-      self.client.delete_blob(path)
+    def rm(self, path, recursive=False):
+        """
+        Remove a single file, or remove a path recursively
+        """
+        if recursive:
+            self.rmdir(path)
+        else:
+            print(f"Deleting {path}")
+            self.client.delete_blob(path)
 
-  def rmdir(self, path):
-    '''
-    Remove a directory and its contents recursively
-    '''
-    blobs = self.ls_files(path, recursive=True)
-    if not blobs:
-      return
+    def rmdir(self, path):
+        """
+        Remove a directory and its contents recursively
+        """
+        blobs = self.ls_files(path, recursive=True)
+        if not blobs:
+            return
 
-    if not path == '' and not path.endswith('/'):
-      path += '/'
-    blobs = [path + blob for blob in blobs]
-    print(f'Deleting {", ".join(blobs)}')
-    self.client.delete_blobs(*blobs)
+        if not path == "" and not path.endswith("/"):
+            path += "/"
+        blobs = [path + blob for blob in blobs]
+        print(f'Deleting {", ".join(blobs)}')
+        self.client.delete_blobs(*blobs)
+
 
 # Sample setup
 
 import sys
+
 try:
-  CONNECTION_STRING = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+    CONNECTION_STRING = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
 except KeyError:
-  print('AZURE_STORAGE_CONNECTION_STRING must be set')
-  sys.exit(1)
+    print("AZURE_STORAGE_CONNECTION_STRING must be set")
+    sys.exit(1)
 
 try:
-  CONTAINER_NAME = sys.argv[1]
+    CONTAINER_NAME = sys.argv[1]
 except IndexError:
-  print('usage: directory_interface.py CONTAINER_NAME')
-  print('error: the following arguments are required: CONTAINER_NAME')
-  sys.exit(1)
+    print("usage: directory_interface.py CONTAINER_NAME")
+    print("error: the following arguments are required: CONTAINER_NAME")
+    sys.exit(1)
 
-SAMPLE_DIRS = [
-  'cats/calico',
-  'cats/siamese',
-  'cats/tabby'
-]
+SAMPLE_DIRS = ["cats/calico", "cats/siamese", "cats/tabby"]
 
 SAMPLE_FILES = [
-  'readme.txt',
-  'cats/herds.txt',
-  'cats/calico/anna.txt',
-  'cats/calico/felix.txt',
-  'cats/siamese/mocha.txt',
-  'cats/tabby/bojangles.txt'
+    "readme.txt",
+    "cats/herds.txt",
+    "cats/calico/anna.txt",
+    "cats/calico/felix.txt",
+    "cats/siamese/mocha.txt",
+    "cats/tabby/bojangles.txt",
 ]
 
 for path in SAMPLE_DIRS:
-  os.makedirs(path, exist_ok=True)
+    os.makedirs(path, exist_ok=True)
 
 for path in SAMPLE_FILES:
-  with open(path, 'w') as file:
-    file.write('content')
+    with open(path, "w") as file:
+        file.write("content")
 
 # Sample body
 
@@ -208,8 +207,8 @@ client = DirectoryClient(CONNECTION_STRING, CONTAINER_NAME)
 # After this call, the container will look like:
 #   cat-herding/
 #     readme.txt
-client.upload('readme.txt', 'cat-herding/readme.txt')
-files = client.ls_files('', recursive=True)
+client.upload("readme.txt", "cat-herding/readme.txt")
+files = client.ls_files("", recursive=True)
 print(files)
 
 # Upload a directory to the container with a path prefix. The directory
@@ -227,14 +226,14 @@ print(files)
 #         mocha.txt
 #       tabby/
 #         bojangles.txt
-client.upload('cats', 'cat-herding')
-files = client.ls_files('', recursive=True)
+client.upload("cats", "cat-herding")
+files = client.ls_files("", recursive=True)
 print(files)
 
 # List files in a single directory
 # Returns:
 # ['herds.txt']
-files = client.ls_files('cat-herding/cats')
+files = client.ls_files("cat-herding/cats")
 print(files)
 
 # List files in a directory recursively
@@ -246,19 +245,19 @@ print(files)
 #   'siamese/mocha.txt',
 #   'tabby/bojangles.txt'
 # ]
-files = client.ls_files('cat-herding/cats', recursive=True)
+files = client.ls_files("cat-herding/cats", recursive=True)
 print(files)
 
 # List directories in a single directory
 # Returns:
 # ['calico', 'siamese', 'tabby']
-dirs = client.ls_dirs('cat-herding/cats')
+dirs = client.ls_dirs("cat-herding/cats")
 print(dirs)
 
 # List files in a directory recursively
 # Returns:
 # ['cats', 'cats/calico', 'cats/siamese', 'cats/tabby']
-dirs = client.ls_dirs('cat-herding', recursive=True)
+dirs = client.ls_dirs("cat-herding", recursive=True)
 print(dirs)
 
 # Download a single file to a location on disk, specifying the destination file
@@ -270,9 +269,10 @@ print(dirs)
 # After this call, your working directory will look like:
 #   downloads/
 #     cat-info.txt
-client.download('cat-herding/readme.txt', 'downloads/cat-info.txt')
+client.download("cat-herding/readme.txt", "downloads/cat-info.txt")
 import glob
-print(glob.glob('downloads/**', recursive=True))
+
+print(glob.glob("downloads/**", recursive=True))
 
 # Download a single file to a folder on disk, preserving the original file name.
 # When the destination ends with a slash '/' or is a relative path specifier
@@ -286,8 +286,8 @@ print(glob.glob('downloads/**', recursive=True))
 #     cat-info.txt
 #     herd-info/
 #       herds.txt
-client.download('cat-herding/cats/herds.txt', 'downloads/herd-info/')
-print(glob.glob('downloads/**', recursive=True))
+client.download("cat-herding/cats/herds.txt", "downloads/herd-info/")
+print(glob.glob("downloads/**", recursive=True))
 
 # Download a directory to a folder on disk. The destination is always
 # interpreted as a directory name. The directory structure will be preserved
@@ -309,8 +309,8 @@ print(glob.glob('downloads/**', recursive=True))
 #     cat-info.txt
 #     herd-info/
 #       herds.txt
-client.download('cat-herding/cats', 'downloads/cat-data')
-print(glob.glob('downloads/**', recursive=True))
+client.download("cat-herding/cats", "downloads/cat-data")
+print(glob.glob("downloads/**", recursive=True))
 
 # Delete a single file from the container
 #
@@ -325,8 +325,8 @@ print(glob.glob('downloads/**', recursive=True))
 #         mocha.txt
 #       tabby/
 #         bojangles.txt
-client.rm('cat-herding/cats/calico/felix.txt')
-files = client.ls_files('', recursive=True)
+client.rm("cat-herding/cats/calico/felix.txt")
+files = client.ls_files("", recursive=True)
 print(files)
 
 # Delete files in a directory recursively. This is equivalent to
@@ -335,21 +335,22 @@ print(files)
 # After this call, the container will look like:
 #   cat-herding/
 #     readme.txt
-client.rm('cat-herding/cats', recursive=True)
-files = client.ls_files('', recursive=True)
+client.rm("cat-herding/cats", recursive=True)
+files = client.ls_files("", recursive=True)
 print(files)
 
 # Delete files in a directory recursively. This is equivalent to
 # client.rm('cat-herding', recursive=True)
 #
 # After this call, the container will be empty.
-client.rmdir('cat-herding')
-files = client.ls_files('', recursive=True)
+client.rmdir("cat-herding")
+files = client.ls_files("", recursive=True)
 print(files)
 
 # Sample cleanup
 
 import shutil
-shutil.rmtree('downloads')
-shutil.rmtree('cats')
-os.remove('readme.txt')
+
+shutil.rmtree("downloads")
+shutil.rmtree("cats")
+os.remove("readme.txt")
