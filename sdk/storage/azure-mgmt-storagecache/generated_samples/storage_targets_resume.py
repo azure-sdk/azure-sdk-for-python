809c19737cfc3b1c1f6d52aae43cf482880f04a1
SDKAuto <sdkautomation@microsoft.com>
2024-10-21 15:12:49 +0000

    CodeGen from PR 28910 in Azure/azure-rest-api-specs Merge 4157191a0140ae26059b304af786a8bfa1bdf2c9 into 362baa4049e19f5a01c3d18760e17f5f22583276

--- sdk/storage/azure-mgmt-storagecache/generated_samples/storage_targets_resume.py
+++ sdk/storage/azure-mgmt-storagecache/generated_samples/storage_targets_resume.py
@@ -37,6 +37,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2024-03-01/examples/StorageTargets_Resume.json
+# x-ms-original-file: specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2024-07-01/examples/StorageTargets_Resume.json
 if __name__ == "__main__":
     main()
