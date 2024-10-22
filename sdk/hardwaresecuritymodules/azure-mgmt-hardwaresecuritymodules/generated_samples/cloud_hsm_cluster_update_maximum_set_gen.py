87b1847075d3a74e404d6e0e92efe2830d619689
SDKAuto <sdkautomation@microsoft.com>
2024-10-22 16:17:43 +0000

    CodeGen from PR 31151 in Azure/azure-rest-api-specs Merge 6f419d2801cad2a5cc709e01e483fb11c4cec0bb into d1dc8a3c92c2725c4854f9c4189b08fe3c0ca4c8

--- sdk/hardwaresecuritymodules/azure-mgmt-hardwaresecuritymodules/generated_samples/cloud_hsm_cluster_update_maximum_set_gen.py
+++ sdk/hardwaresecuritymodules/azure-mgmt-hardwaresecuritymodules/generated_samples/cloud_hsm_cluster_update_maximum_set_gen.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.hardwaresecuritymodules import HardwareSecurityModulesMgmtClient
 
 """
@@ -36,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2023-12-10-preview/examples/CloudHsmCluster_Update_MaximumSet_Gen.json
+# x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/stable/2025-03-31/examples/CloudHsmCluster_Update_MaximumSet_Gen.json
 if __name__ == "__main__":
     main()
