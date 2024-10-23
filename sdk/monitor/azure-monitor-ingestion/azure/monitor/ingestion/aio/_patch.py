1cf6ec6042bdda43de95b2d1d3a16c48b6147694
SDKAuto <sdkautomation@microsoft.com>
2024-10-23 00:58:58 +0000

    CodeGen from PR 30378 in Azure/azure-rest-api-specs Merge 4ed5b8a9e5e7abaca1a22f011be216cdbf1babba into 66fefe4b93b3f1ab515757e48597a9205c924879

--- sdk/monitor/azure-monitor-ingestion/azure/monitor/ingestion/aio/_patch.py
+++ sdk/monitor/azure-monitor-ingestion/azure/monitor/ingestion/aio/_patch.py
@@ -7,42 +7,8 @@
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
 from typing import List
-from ._client import LogsIngestionClient as GeneratedClient
 
-
-class LogsIngestionClient(GeneratedClient):
-    """The asynchronous client for uploading logs to Azure Monitor.
-
-    :param endpoint: The Data Collection Endpoint for the Data Collection Rule, for example
-     https://dce-name.eastus-2.ingest.monitor.azure.com.
-    :type endpoint: str
-    :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
-    :keyword api_version: Api Version. Default value is "2023-01-01". Note that overriding
-     this default value may result in unsupported behavior.
-    :paramtype api_version: str
-
-    .. admonition:: Example:
-
-        .. literalinclude:: ../samples/async_samples/sample_authentication_async.py
-            :start-after: [START create_client_public_cloud_async]
-            :end-before: [END create_client_public_cloud_async]
-            :language: python
-            :dedent: 4
-            :caption: Creating the LogsIngestionClient with DefaultAzureCredential.
-
-    .. admonition:: Example:
-
-       .. literalinclude:: ../samples/async_samples/sample_authentication_async.py
-            :start-after: [START create_client_sovereign_cloud_async]
-            :end-before: [END create_client_sovereign_cloud_async]
-            :language: python
-            :dedent: 4
-            :caption: Creating the LogsIngestionClient for use with a sovereign cloud (i.e. non-public cloud).
-    """
-
-
-__all__: List[str] = ["LogsIngestionClient"]
+__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
