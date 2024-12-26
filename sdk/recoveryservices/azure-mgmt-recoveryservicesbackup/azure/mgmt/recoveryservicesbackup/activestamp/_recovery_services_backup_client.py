581d75bb3ba04fac9aba77e4500ba52181439671
SDKAuto <sdkautomation@microsoft.com>
2024-12-26 07:54:15 +0000

    CodeGen from PR 31959 in Azure/azure-rest-api-specs Merge 12a7fb8e8b1411718ad6325db3c6cbc4bf0b597e into 64f6997950d8bee63f314563cedaa20148db2186

--- sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/azure/mgmt/recoveryservicesbackup/activestamp/_recovery_services_backup_client.py
+++ sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/azure/mgmt/recoveryservicesbackup/activestamp/_recovery_services_backup_client.py
@@ -8,6 +8,7 @@
 
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
+from typing_extensions import Self
 
 from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
@@ -74,13 +75,12 @@ from .operations import (
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
 
 class RecoveryServicesBackupClient(
     RecoveryServicesBackupClientOperationsMixin
-):  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
+):  # pylint: disable=too-many-instance-attributes
     """Open API 2.0 Specs for Azure RecoveryServices Backup service.
 
     :ivar backup_resource_storage_configs_non_crr: BackupResourceStorageConfigsNonCRROperations
@@ -443,7 +443,7 @@ class RecoveryServicesBackupClient(
     def close(self) -> None:
         self._client.close()
 
-    def __enter__(self) -> "RecoveryServicesBackupClient":
+    def __enter__(self) -> Self:
         self._client.__enter__()
         return self
 
