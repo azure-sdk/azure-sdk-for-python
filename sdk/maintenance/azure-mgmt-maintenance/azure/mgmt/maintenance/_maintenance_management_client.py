798751290d3c6baa4c0a187bc13634819867aabd
SDKAuto <sdkautomation@microsoft.com>
2025-01-08 06:19:47 +0000

    CodeGen from PR 31941 in Azure/azure-rest-api-specs Merge a2184fa132a9c46948a0d938431b92ca95f7e751 into 63105f9cb33e0da383e9069ef3632035ee804e01

--- sdk/maintenance/azure-mgmt-maintenance/azure/mgmt/maintenance/_maintenance_management_client.py
+++ sdk/maintenance/azure-mgmt-maintenance/azure/mgmt/maintenance/_maintenance_management_client.py
@@ -8,6 +8,7 @@
 
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
+from typing_extensions import Self
 
 from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
@@ -33,11 +34,10 @@ from .operations import (
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
 
-class MaintenanceManagementClient:  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
+class MaintenanceManagementClient:  # pylint: disable=too-many-instance-attributes
     """Azure Maintenance Management Client.
 
     :ivar scheduled_event: ScheduledEventOperations operations
@@ -175,7 +175,7 @@ class MaintenanceManagementClient:  # pylint: disable=client-accepts-api-version
     def close(self) -> None:
         self._client.close()
 
-    def __enter__(self) -> "MaintenanceManagementClient":
+    def __enter__(self) -> Self:
         self._client.__enter__()
         return self
 
