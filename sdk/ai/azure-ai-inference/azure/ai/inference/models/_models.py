ac689b6e98a0c20f45128ccf5896515d6f24cf73
SDKAuto <sdkautomation@microsoft.com>
2025-01-22 23:08:56 +0000

    CodeGen from PR 32287 in Azure/azure-rest-api-specs Merge a20f200b948220835f367aed20d09c3914474192 into 7c962ee95ab11d1f511f2c46773a1ae6a9aca684

--- sdk/ai/azure-ai-inference/azure/ai/inference/models/_models.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/models/_models.py
@@ -130,6 +130,8 @@ class ChatCompletionsNamedToolChoice(_model_base.Model):
 
     Readonly variables are only populated by the server, and will be ignored when sending a request.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar type: The type of the tool. Currently, only ``function`` is supported. Required. Default
      value is "function".
     :vartype type: str
@@ -165,6 +167,8 @@ class ChatCompletionsNamedToolChoice(_model_base.Model):
 class ChatCompletionsNamedToolChoiceFunction(_model_base.Model):
     """A tool selection of a specific, named function tool that will limit chat completions to using
     the named function.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar name: The name of the function that should be called. Required.
     :vartype name: str
@@ -202,6 +206,8 @@ class ChatCompletionsResponseFormat(_model_base.Model):
     ChatCompletionsResponseFormatJsonObject, ChatCompletionsResponseFormatJsonSchema,
     ChatCompletionsResponseFormatText
 
+    All required parameters must be populated in order to send to server.
+
     :ivar type: The response format type to use for chat completions. Required. Default value is
      None.
     :vartype type: str
@@ -235,6 +241,8 @@ class ChatCompletionsResponseFormatJsonObject(ChatCompletionsResponseFormat, dis
     produce JSON
     via a system or user message.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar type: Response format type: always 'json_object' for this object. Required. Default value
      is "json_object".
     :vartype type: str
@@ -264,6 +272,8 @@ class ChatCompletionsResponseFormatJsonSchema(ChatCompletionsResponseFormat, dis
     """A response format for Chat Completions that restricts responses to emitting valid JSON objects,
     with a
     JSON schema specified by the caller.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar type: The type of response format being defined: ``json_schema``. Required. Default value
      is "json_schema".
@@ -300,6 +310,8 @@ class ChatCompletionsResponseFormatJsonSchema(ChatCompletionsResponseFormat, dis
 class ChatCompletionsResponseFormatText(ChatCompletionsResponseFormat, discriminator="text"):
     """A response format for Chat Completions that emits text responses. This is the default response
     format.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar type: Response format type: always 'text' for this object. Required. Default value is
      "text".
@@ -373,6 +385,8 @@ class ChatCompletionsToolDefinition(_model_base.Model):
 
     Readonly variables are only populated by the server, and will be ignored when sending a request.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar type: The type of the tool. Currently, only ``function`` is supported. Required. Default
      value is "function".
     :vartype type: str
@@ -405,12 +419,120 @@ class ChatCompletionsToolDefinition(_model_base.Model):
         self.type: Literal["function"] = "function"
 
 
+class ContentItem(_model_base.Model):
+    """An abstract representation of a structured content item within a chat message.
+
+    You probably want to use the sub-classes and not this class directly. Known sub-classes are:
+    ImageContentItem, ChatMessageAudioContentItem, TextContentItem
+
+    All required parameters must be populated in order to send to server.
+
+    :ivar type: The discriminated object type. Required. Default value is None.
+    :vartype type: str
+    """
+
+    __mapping__: Dict[str, _model_base.Model] = {}
+    type: str = rest_discriminator(name="type")
+    """The discriminated object type. Required. Default value is None."""
+
+    @overload
+    def __init__(
+        self,
+        *,
+        type: str,
+    ) -> None: ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]) -> None:
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class ChatMessageAudioContentItem(ContentItem, discriminator="input_audio"):
+    """A structured chat content item containing an audio content.
+
+    All required parameters must be populated in order to send to server.
+
+    :ivar type: The discriminated object type: always 'input_audio' for this type. Required.
+     Default value is "input_audio".
+    :vartype type: str
+    :ivar input_audio: The details of the input audio. Required.
+    :vartype input_audio: ~azure.ai.inference.models.ChatMessageInputAudio
+    """
+
+    type: Literal["input_audio"] = rest_discriminator(name="type")  # type: ignore
+    """The discriminated object type: always 'input_audio' for this type. Required. Default value is
+     \"input_audio\"."""
+    input_audio: "_models.ChatMessageInputAudio" = rest_field()
+    """The details of the input audio. Required."""
+
+    @overload
+    def __init__(
+        self,
+        *,
+        input_audio: "_models.ChatMessageInputAudio",
+    ) -> None: ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]) -> None:
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, type="input_audio", **kwargs)
+
+
+class ChatMessageInputAudio(_model_base.Model):
+    """The details of an audio chat message content part.
+
+    All required parameters must be populated in order to send to server.
+
+    :ivar data: Base64 encoded audio data. Required.
+    :vartype data: str
+    :ivar format: The audio format of the audio content. Required. Known values are: "wav" and
+     "mp3".
+    :vartype format: str or ~azure.ai.inference.models.AudioContentFormat
+    """
+
+    data: str = rest_field()
+    """Base64 encoded audio data. Required."""
+    format: Union[str, "_models.AudioContentFormat"] = rest_field()
+    """The audio format of the audio content. Required. Known values are: \"wav\" and \"mp3\"."""
+
+    @overload
+    def __init__(
+        self,
+        *,
+        data: str,
+        format: Union[str, "_models.AudioContentFormat"],
+    ) -> None: ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]) -> None:
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+
 class ChatRequestMessage(_model_base.Model):
     """An abstract representation of a chat message as provided in a request.
 
     You probably want to use the sub-classes and not this class directly. Known sub-classes are:
     ChatRequestAssistantMessage, ChatRequestSystemMessage, ChatRequestToolMessage,
     ChatRequestUserMessage
+
+    All required parameters must be populated in order to send to server.
 
     :ivar role: The chat role associated with this message. Required. Known values are: "system",
      "user", "assistant", and "tool".
@@ -442,6 +564,8 @@ class ChatRequestMessage(_model_base.Model):
 
 class ChatRequestAssistantMessage(ChatRequestMessage, discriminator="assistant"):
     """A request chat message representing response or action from the assistant.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar role: The chat role associated with this message, which is always 'assistant' for
      assistant messages. Required. The role that provides responses to system-instructed,
@@ -489,6 +613,8 @@ class ChatRequestSystemMessage(ChatRequestMessage, discriminator="system"):
     generate a chat completions
     response.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar role: The chat role associated with this message, which is always 'system' for system
      messages. Required. The role that instructs or sets the behavior of the assistant.
     :vartype role: str or ~azure.ai.inference.models.SYSTEM
@@ -522,6 +648,8 @@ class ChatRequestSystemMessage(ChatRequestMessage, discriminator="system"):
 
 class ChatRequestToolMessage(ChatRequestMessage, discriminator="tool"):
     """A request chat message representing requested output from a configured tool.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar role: The chat role associated with this message, which is always 'tool' for tool
      messages. Required. The role that represents extension tool activity within a chat completions
@@ -563,6 +691,8 @@ class ChatRequestToolMessage(ChatRequestMessage, discriminator="tool"):
 class ChatRequestUserMessage(ChatRequestMessage, discriminator="user"):
     """A request chat message representing user input to the assistant.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar role: The chat role associated with this message, which is always 'user' for user
      messages. Required. The role that provides input for chat completions.
     :vartype role: str or ~azure.ai.inference.models.USER
@@ -574,7 +704,7 @@ class ChatRequestUserMessage(ChatRequestMessage, discriminator="user"):
     role: Literal[ChatRole.USER] = rest_discriminator(name="role")  # type: ignore
     """The chat role associated with this message, which is always 'user' for user messages. Required.
      The role that provides input for chat completions."""
-    content: Union["str", List["_models.ContentItem"]] = rest_field()
+    content: Union[str, List["_models.ContentItem"]] = rest_field()
     """The contents of the user message, with available input types varying by selected model.
      Required. Is either a str type or a [ContentItem] type."""
 
@@ -685,38 +815,6 @@ class CompletionsUsage(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class ContentItem(_model_base.Model):
-    """An abstract representation of a structured content item within a chat message.
-
-    You probably want to use the sub-classes and not this class directly. Known sub-classes are:
-    ImageContentItem, TextContentItem
-
-    :ivar type: The discriminated object type. Required. Default value is None.
-    :vartype type: str
-    """
-
-    __mapping__: Dict[str, _model_base.Model] = {}
-    type: str = rest_discriminator(name="type")
-    """The discriminated object type. Required. Default value is None."""
-
-    @overload
-    def __init__(
-        self,
-        *,
-        type: str,
-    ) -> None: ...
-
-    @overload
-    def __init__(self, mapping: Mapping[str, Any]) -> None:
-        """
-        :param mapping: raw JSON to initialize the model.
-        :type mapping: Mapping[str, Any]
-        """
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-
-
 class EmbeddingItem(_model_base.Model):
     """Representation of a single embeddings relatedness comparison.
 
@@ -730,7 +828,7 @@ class EmbeddingItem(_model_base.Model):
     :vartype index: int
     """
 
-    embedding: Union["str", List[float]] = rest_field()
+    embedding: Union[str, List[float]] = rest_field()
     """List of embedding values for the input prompt. These represent a measurement of the
      vector-based relatedness of the provided input. Or a base64 encoded string of the embedding
      vector. Required. Is either a str type or a [float] type."""
@@ -885,6 +983,8 @@ class FunctionDefinition(_model_base.Model):
     """The definition of a caller-specified function that chat completions may invoke in response to
     matching user input.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar name: The name of the function to be called. Required.
     :vartype name: str
     :ivar description: A description of what the function does. The model will use this description
@@ -927,6 +1027,8 @@ class FunctionDefinition(_model_base.Model):
 class ImageContentItem(ContentItem, discriminator="image_url"):
     """A structured chat content item containing an image reference.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar type: The discriminated object type: always 'image_url' for this type. Required. Default
      value is "image_url".
     :vartype type: str
@@ -962,6 +1064,8 @@ class ImageContentItem(ContentItem, discriminator="image_url"):
 
 class ImageEmbeddingInput(_model_base.Model):
     """Represents an image with optional text.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar image: The input image encoded in base64 string as a data URL. Example:
      ``data:image/{format};base64,{data}``. Required.
@@ -1000,6 +1104,8 @@ class ImageEmbeddingInput(_model_base.Model):
 class ImageUrl(_model_base.Model):
     """An internet location from which the model may retrieve an image.
 
+    All required parameters must be populated in order to send to server.
+
     :ivar url: The URL of the image. Required.
     :vartype url: str
     :ivar detail: The evaluation quality setting to use, which controls relative prioritization of
@@ -1037,6 +1143,8 @@ class ImageUrl(_model_base.Model):
 class JsonSchemaFormat(_model_base.Model):
     """Defines the response format for chat completions as JSON with a given schema.
     The AI model will need to adhere to this schema when generating completions.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar name: A name that labels this JSON schema. Must be a-z, A-Z, 0-9, or contain underscores
      and dashes, with a maximum length of 64. Required.
@@ -1327,6 +1435,8 @@ class StreamingChatResponseToolCallUpdate(_model_base.Model):
 
 class TextContentItem(ContentItem, discriminator="text"):
     """A structured chat content item containing plain text.
+
+    All required parameters must be populated in order to send to server.
 
     :ivar type: The discriminated object type: always 'text' for this type. Required. Default value
      is "text".
