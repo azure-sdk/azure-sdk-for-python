171f62b8ec185253bf9dc72201835f3dc1083737
SDKAuto <sdkautomation@microsoft.com>
2025-01-30 17:09:49 +0000

    CodeGen from PR 32339 in Azure/azure-rest-api-specs Merge aeff684cf1e913ea4de43ea4256ec1f5dd7e8675 into 81330ee21a8300ad377eacbaaf16efabb91d56c0

--- sdk/quantum/azure-mgmt-quantum/azure/mgmt/quantum/_azure_quantum_mgmt_client.py
+++ sdk/quantum/azure-mgmt-quantum/azure/mgmt/quantum/_azure_quantum_mgmt_client.py
@@ -8,31 +8,31 @@
 
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
+from typing_extensions import Self
 
+from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core.policies import ARMAutoResourceProviderRegistrationPolicy
 
 from . import models as _models
 from ._configuration import AzureQuantumMgmtClientConfiguration
 from ._serialization import Deserializer, Serializer
-from .operations import OfferingsOperations, Operations, WorkspaceOperations, WorkspacesOperations
+from .operations import OfferingsOperations, Operations, WorkspacesOperations
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
 
-class AzureQuantumMgmtClient:  # pylint: disable=client-accepts-api-version-keyword
-    """AzureQuantumMgmtClient.
+class AzureQuantumMgmtClient:
+    """Microsoft.Quantum Resource Provider Management API.
 
+    :ivar operations: Operations operations
+    :vartype operations: azure.mgmt.quantum.operations.Operations
     :ivar workspaces: WorkspacesOperations operations
     :vartype workspaces: azure.mgmt.quantum.operations.WorkspacesOperations
     :ivar offerings: OfferingsOperations operations
     :vartype offerings: azure.mgmt.quantum.operations.OfferingsOperations
-    :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.quantum.operations.Operations
-    :ivar workspace: WorkspaceOperations operations
-    :vartype workspace: azure.mgmt.quantum.operations.WorkspaceOperations
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
     :param subscription_id: The ID of the target subscription. The value must be an UUID. Required.
@@ -56,18 +56,35 @@ class AzureQuantumMgmtClient:  # pylint: disable=client-accepts-api-version-keyw
         self._config = AzureQuantumMgmtClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                ARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
+        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
         self.workspaces = WorkspacesOperations(self._client, self._config, self._serialize, self._deserialize)
         self.offerings = OfferingsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
-        self.workspace = WorkspaceOperations(self._client, self._config, self._serialize, self._deserialize)
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, *, stream: bool = False, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -87,12 +104,12 @@ class AzureQuantumMgmtClient:  # pylint: disable=client-accepts-api-version-keyw
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     def close(self) -> None:
         self._client.close()
 
-    def __enter__(self) -> "AzureQuantumMgmtClient":
+    def __enter__(self) -> Self:
         self._client.__enter__()
         return self
 
