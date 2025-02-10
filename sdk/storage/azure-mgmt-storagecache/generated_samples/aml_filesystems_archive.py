bc4b1bb0a366870319216ed3c9464916afc5899a
SDKAuto <sdkautomation@microsoft.com>
2025-02-10 13:27:12 +0000

    CodeGen from PR 32537 in Azure/azure-rest-api-specs Merge 07db43ecaca93459531cda8152dbe79670b15cf6 into 53b2029545881f27d0f0fad3892065f39b1b9e83

--- sdk/storage/azure-mgmt-storagecache/generated_samples/aml_filesystems_archive.py
+++ sdk/storage/azure-mgmt-storagecache/generated_samples/aml_filesystems_archive.py
@@ -36,6 +36,6 @@ def main():
     )
 
 
-# x-ms-original-file: specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2024-03-01/examples/amlFilesystems_Archive.json
+# x-ms-original-file: specification/storagecache/resource-manager/Microsoft.StorageCache/stable/2024-07-01/examples/amlFilesystems_Archive.json
 if __name__ == "__main__":
     main()
