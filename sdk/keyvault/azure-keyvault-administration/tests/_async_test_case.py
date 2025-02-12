787739817a2151fd22a2f0f626f8c65457677408
SDKAuto <sdkautomation@microsoft.com>
2025-02-12 00:08:27 +0000

    CodeGen from PR 32571 in Azure/azure-rest-api-specs Merge e3ceb343be033be4e3407c48df2f1d6e00952ae4 into 1aafda2706962b6f58ad1fb07087b5320350e619

--- sdk/keyvault/azure-keyvault-administration/tests/_async_test_case.py
+++ sdk/keyvault/azure-keyvault-administration/tests/_async_test_case.py
@@ -25,7 +25,7 @@ class BaseClientPreparer(AzureRecordedTestCase):
             self.container_uri = f"{storage_url}/{container_name}"
 
             self.sas_token = os.environ.get("BLOB_STORAGE_SAS_TOKEN")
-            
+
         else:
             self.managed_hsm_url = hsm_playback_url
             self.container_uri = container_playback_uri
@@ -39,7 +39,7 @@ class BaseClientPreparer(AzureRecordedTestCase):
         # Only set service principal credentials if user-based auth is not requested
         if use_pwsh == use_cli == use_vscode == use_azd == "false":
             self._set_mgmt_settings_real_values()
-    
+
     def _skip_if_not_configured(self, api_version, **kwargs):
         if self.is_live and api_version != DEFAULT_VERSION:
             pytest.skip("This test only uses the default API version for live tests")
@@ -63,6 +63,7 @@ class KeyVaultBackupClientPreparer(BaseClientPreparer):
 
             async with client:
                 await fn(test_class, client, **kwargs)
+
         return _preparer
 
     def create_backup_client(self, managed_identity_client_id, **kwargs):
@@ -88,6 +89,7 @@ class KeyVaultBackupClientSasPreparer(BaseClientPreparer):
 
             async with client:
                 await fn(test_class, client, **kwargs)
+
         return _preparer
 
     def create_backup_client(self, **kwargs):
@@ -107,11 +109,11 @@ class KeyVaultAccessControlClientPreparer(BaseClientPreparer):
 
             async with client:
                 await fn(test_class, client, **kwargs)
+
         return _preparer
 
     def create_access_control_client(self, **kwargs):
-        from azure.keyvault.administration.aio import \
-            KeyVaultAccessControlClient
+        from azure.keyvault.administration.aio import KeyVaultAccessControlClient
 
         credential = self.get_credential(KeyVaultAccessControlClient, is_async=True)
         return self.create_client_from_credential(
@@ -127,11 +129,11 @@ class KeyVaultSettingsClientPreparer(BaseClientPreparer):
 
             async with client:
                 await fn(test_class, client, **kwargs)
+
         return _preparer
 
     def create_access_control_client(self, **kwargs):
-        from azure.keyvault.administration.aio import \
-            KeyVaultSettingsClient
+        from azure.keyvault.administration.aio import KeyVaultSettingsClient
 
         credential = self.get_credential(KeyVaultSettingsClient, is_async=True)
         return self.create_client_from_credential(
