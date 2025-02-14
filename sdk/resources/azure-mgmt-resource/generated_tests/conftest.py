5649d971e48bd590efad7f0d05add4b612c91597
SDKAuto <sdkautomation@microsoft.com>
2025-02-14 03:11:23 +0000

    CodeGen from PR 32632 in Azure/azure-rest-api-specs Merge d4f796ea6470118889be3d57a9fe529b96aafae2 into eb18e6dd159fe03abb1250f90a3844058a248b19

--- sdk/resources/azure-mgmt-resource/generated_tests/conftest.py
+++ sdk/resources/azure-mgmt-resource/generated_tests/conftest.py
@@ -18,17 +18,17 @@ from devtools_testutils import (
 load_dotenv()
 
 
-# aovid record sensitive identity information in recordings
+# For security, please avoid record sensitive identity information in recordings
 @pytest.fixture(scope="session", autouse=True)
 def add_sanitizers(test_proxy):
-    databoundarymgmt_subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
-    databoundarymgmt_tenant_id = os.environ.get("AZURE_TENANT_ID", "00000000-0000-0000-0000-000000000000")
-    databoundarymgmt_client_id = os.environ.get("AZURE_CLIENT_ID", "00000000-0000-0000-0000-000000000000")
-    databoundarymgmt_client_secret = os.environ.get("AZURE_CLIENT_SECRET", "00000000-0000-0000-0000-000000000000")
-    add_general_regex_sanitizer(regex=databoundarymgmt_subscription_id, value="00000000-0000-0000-0000-000000000000")
-    add_general_regex_sanitizer(regex=databoundarymgmt_tenant_id, value="00000000-0000-0000-0000-000000000000")
-    add_general_regex_sanitizer(regex=databoundarymgmt_client_id, value="00000000-0000-0000-0000-000000000000")
-    add_general_regex_sanitizer(regex=databoundarymgmt_client_secret, value="00000000-0000-0000-0000-000000000000")
+    bicep_subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+    bicep_tenant_id = os.environ.get("AZURE_TENANT_ID", "00000000-0000-0000-0000-000000000000")
+    bicep_client_id = os.environ.get("AZURE_CLIENT_ID", "00000000-0000-0000-0000-000000000000")
+    bicep_client_secret = os.environ.get("AZURE_CLIENT_SECRET", "00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=bicep_subscription_id, value="00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=bicep_tenant_id, value="00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=bicep_client_id, value="00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=bicep_client_secret, value="00000000-0000-0000-0000-000000000000")
 
     add_header_regex_sanitizer(key="Set-Cookie", value="[set-cookie;]")
     add_header_regex_sanitizer(key="Cookie", value="cookie;")
