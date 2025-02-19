e58f466f16dfbfaeb0ba0894d80deca903b1fb9f
SDKAuto <sdkautomation@microsoft.com>
2025-02-19 13:53:58 +0000

    CodeGen from PR 32419 in Azure/azure-rest-api-specs Merge b13898b4f362c7ec80fc13fa38383e69813b6dd0 into af7e6643187e9214a0306b310267295b87b4cec6

--- sdk/containerinstance/azure-mgmt-containerinstance/generated_samples/container_exec.py
+++ sdk/containerinstance/azure-mgmt-containerinstance/generated_samples/container_exec.py
@@ -39,6 +39,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2024-05-01-preview/examples/ContainerExec.json
+# x-ms-original-file: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/preview/2024-11-01-preview/examples/ContainerExec.json
 if __name__ == "__main__":
     main()
