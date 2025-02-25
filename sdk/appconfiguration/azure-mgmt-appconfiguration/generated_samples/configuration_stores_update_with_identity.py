680192376e08fed773d92a85b736d23727c03157
SDKAuto <sdkautomation@microsoft.com>
2025-02-25 20:11:51 +0000

    CodeGen from PR 32208 in Azure/azure-rest-api-specs Merge ae445b5a05572e0dfc1fc8fca7e06ac99d604a93 into fcd1c4977ad31417ab298205e87727c4cc8474f1

--- sdk/appconfiguration/azure-mgmt-appconfiguration/generated_samples/configuration_stores_update_with_identity.py
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/generated_samples/configuration_stores_update_with_identity.py
@@ -47,6 +47,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-05-01/examples/ConfigurationStoresUpdateWithIdentity.json
+# x-ms-original-file: specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/stable/2024-06-01/examples/ConfigurationStoresUpdateWithIdentity.json
 if __name__ == "__main__":
     main()
