ba4a6b6090c80bc4f5857736754195d6953f3210
SDKAuto <sdkautomation@microsoft.com>
2025-02-28 14:31:44 +0000

    CodeGen from PR 32911 in Azure/azure-rest-api-specs Merge fa913f3b498d22616368c14eae869ad3a4c4e05e into 69339d9098a3110738a872aa0326246670f0b3f1

--- sdk/trustedsigning/azure-mgmt-trustedsigning/generated_samples/certificate_profiles_create.py
+++ sdk/trustedsigning/azure-mgmt-trustedsigning/generated_samples/certificate_profiles_create.py
@@ -46,6 +46,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: 2024-02-05-preview/CertificateProfiles_Create.json
+# x-ms-original-file: 2025-03-30/CertificateProfiles_Create.json
 if __name__ == "__main__":
     main()
