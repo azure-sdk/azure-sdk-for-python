2ad08b123b030c0bf712eea0ed4e1445730b576a
SDKAuto <sdkautomation@microsoft.com>
2025-03-20 09:46:58 +0000

    CodeGen from PR 33345 in Azure/azure-rest-api-specs Merge 2d5efff31757eec3446a07bf99d10fbe80daa5a4 into 8101a5323ca718856b5d462446107fdeabf28c6d

--- sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/models/_patch.py
+++ sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/models/_patch.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License.
 # ------------------------------------
 """Customize generated code here.
+
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
 from typing import List
@@ -12,6 +13,7 @@ __all__: List[str] = []  # Add all objects you want publicly available to users 
 
 def patch_sdk():
     """Do not remove from this file.
+
     `patch_sdk` is a last resort escape hatch that allows you to do customizations
     you can't accomplish using the techniques described in
     https://aka.ms/azsdk/python/dpcodegen/python/customize
