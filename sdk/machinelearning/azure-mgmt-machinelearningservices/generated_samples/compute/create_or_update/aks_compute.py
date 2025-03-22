57f46754f0a17afd42f4a19c592a6ced33f89298
SDKAuto <sdkautomation@microsoft.com>
2025-03-22 00:05:16 +0000

    CodeGen from PR 33329 in Azure/azure-rest-api-specs Merge ca4879cc1f97fc0e6a584ba9878bb367fd2f0651 into dcb3e92f5bfc2a95a3e995d9048f1de4502ed078

--- sdk/machinelearning/azure-mgmt-machinelearningservices/generated_samples/compute/create_or_update/aks_compute.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/generated_samples/compute/create_or_update/aks_compute.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.machinelearningservices import MachineLearningServicesMgmtClient
 
 """
@@ -46,6 +47,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2023-04-01/examples/Compute/createOrUpdate/AKSCompute.json
+# x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/preview/2025-04-01-preview/examples/Compute/createOrUpdate/AKSCompute.json
 if __name__ == "__main__":
     main()
--- sdk/machinelearning/azure-mgmt-machinelearningservices/generated_samples/registries/remove_regions.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/generated_samples/registries/remove_regions.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.machinelearningservices import MachineLearningServicesMgmtClient
 
 """
@@ -93,6 +94,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2023-04-01/examples/Registries/removeRegions.json
+# x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/preview/2025-04-01-preview/examples/Registries/removeRegions.json
 if __name__ == "__main__":
     main()
