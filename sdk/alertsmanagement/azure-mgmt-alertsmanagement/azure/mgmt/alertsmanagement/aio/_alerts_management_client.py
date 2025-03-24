fa68701b966a1132c29b459e4f0214f859dc7222
SDKAuto <sdkautomation@microsoft.com>
2025-03-24 05:11:46 +0000

    CodeGen from PR 33428 in Azure/azure-rest-api-specs Merge 882d2be262dd4ef6dfdbf62777b40a99e496d852 into 25524506300a020e51f5c95bc770aeb6f0c73f74

--- sdk/alertsmanagement/azure-mgmt-alertsmanagement/azure/mgmt/alertsmanagement/aio/_alerts_management_client.py
+++ sdk/alertsmanagement/azure-mgmt-alertsmanagement/azure/mgmt/alertsmanagement/aio/_alerts_management_client.py
@@ -8,32 +8,33 @@
 
 from copy import deepcopy
 from typing import Any, Awaitable, TYPE_CHECKING
+from typing_extensions import Self
 
+from azure.core.pipeline import policies
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core import AsyncARMPipelineClient
+from azure.mgmt.core.policies import AsyncARMAutoResourceProviderRegistrationPolicy
 
 from .. import models as _models
 from .._serialization import Deserializer, Serializer
 from ._configuration import AlertsManagementClientConfiguration
 from .operations import (
     AlertProcessingRulesOperations,
+    AlertRuleRecommendationsOperations,
     AlertsOperations,
+    IssueOperations,
     Operations,
     PrometheusRuleGroupsOperations,
     SmartGroupsOperations,
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
 
-class AlertsManagementClient:  # pylint: disable=client-accepts-api-version-keyword
+class AlertsManagementClient:  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
     """AlertsManagement Client.
 
-    :ivar alert_processing_rules: AlertProcessingRulesOperations operations
-    :vartype alert_processing_rules:
-     azure.mgmt.alertsmanagement.aio.operations.AlertProcessingRulesOperations
     :ivar prometheus_rule_groups: PrometheusRuleGroupsOperations operations
     :vartype prometheus_rule_groups:
      azure.mgmt.alertsmanagement.aio.operations.PrometheusRuleGroupsOperations
@@ -43,8 +44,18 @@ class AlertsManagementClient:  # pylint: disable=client-accepts-api-version-keyw
     :vartype alerts: azure.mgmt.alertsmanagement.aio.operations.AlertsOperations
     :ivar smart_groups: SmartGroupsOperations operations
     :vartype smart_groups: azure.mgmt.alertsmanagement.aio.operations.SmartGroupsOperations
+    :ivar alert_rule_recommendations: AlertRuleRecommendationsOperations operations
+    :vartype alert_rule_recommendations:
+     azure.mgmt.alertsmanagement.aio.operations.AlertRuleRecommendationsOperations
+    :ivar alert_processing_rules: AlertProcessingRulesOperations operations
+    :vartype alert_processing_rules:
+     azure.mgmt.alertsmanagement.aio.operations.AlertProcessingRulesOperations
+    :ivar issue: IssueOperations operations
+    :vartype issue: azure.mgmt.alertsmanagement.aio.operations.IssueOperations
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
+    :param issue_name: The name of the IssueResource. Required.
+    :type issue_name: str
     :param subscription_id: The ID of the target subscription. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
@@ -54,30 +65,55 @@ class AlertsManagementClient:  # pylint: disable=client-accepts-api-version-keyw
     def __init__(
         self,
         credential: "AsyncTokenCredential",
+        issue_name: str,
         subscription_id: str,
         base_url: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
         self._config = AlertsManagementClientConfiguration(
-            credential=credential, subscription_id=subscription_id, **kwargs
+            credential=credential, issue_name=issue_name, subscription_id=subscription_id, **kwargs
         )
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                AsyncARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.alert_processing_rules = AlertProcessingRulesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
         self.prometheus_rule_groups = PrometheusRuleGroupsOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
         self.alerts = AlertsOperations(self._client, self._config, self._serialize, self._deserialize)
         self.smart_groups = SmartGroupsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.alert_rule_recommendations = AlertRuleRecommendationsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.alert_processing_rules = AlertProcessingRulesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.issue = IssueOperations(self._client, self._config, self._serialize, self._deserialize)
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
+    def _send_request(
+        self, request: HttpRequest, *, stream: bool = False, **kwargs: Any
+    ) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -97,14 +133,14 @@ class AlertsManagementClient:  # pylint: disable=client-accepts-api-version-keyw
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     async def close(self) -> None:
         await self._client.close()
 
-    async def __aenter__(self) -> "AlertsManagementClient":
+    async def __aenter__(self) -> Self:
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
