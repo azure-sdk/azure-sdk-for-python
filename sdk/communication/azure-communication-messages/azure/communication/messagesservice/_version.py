95e094d4aed477d3f3cbe799d554d025741f9370
SDKAuto <sdkautomation@microsoft.com>
2025-03-24 17:30:33 +0000

    CodeGen from PR 32282 in Azure/azure-rest-api-specs Merge 3f8b2c6b438fc8bfb90100e29a602e69d8319bd5 into e0ef299f475a972c2856175014bf4f1e9873dda0

--- sdk/communication/azure-communication-messages/azure/communication/messagesservice/_version.py
+++ sdk/communication/azure-communication-messages/azure/communication/messagesservice/_version.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# pylint: disable=too-many-lines
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
@@ -7,6 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Literal
-
-RepeatabilityResult = Literal["accepted", "rejected"]
+VERSION = "1.0.0"
