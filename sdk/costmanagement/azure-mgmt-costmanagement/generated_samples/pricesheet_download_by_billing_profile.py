ae6dcbcaada8462de8d0f8a23da69fa4a1688606
SDKAuto <sdkautomation@microsoft.com>
2025-03-27 17:03:20 +0000

    CodeGen from PR 33523 in Azure/azure-rest-api-specs Merge beb4f69eabbb14cacc608b7d42226caf21a1e1b0 into 0c62abad8eabfcba5c50a0dfdc5778d86bab2130

--- sdk/costmanagement/azure-mgmt-costmanagement/generated_samples/pricesheet_download_by_billing_profile.py
+++ sdk/costmanagement/azure-mgmt-costmanagement/generated_samples/pricesheet_download_by_billing_profile.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.costmanagement import CostManagementClient
 
 """
@@ -35,6 +36,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/cost-management/resource-manager/Microsoft.CostManagement/stable/2022-10-01/examples/PricesheetDownloadByBillingProfile.json
+# x-ms-original-file: specification/cost-management/resource-manager/Microsoft.CostManagement/stable/2025-03-01/examples/PricesheetDownloadByBillingProfile.json
 if __name__ == "__main__":
     main()
