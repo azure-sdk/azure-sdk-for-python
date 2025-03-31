5eb583f1bcda05b43fc923f4139fa36f305e9331
SDKAuto <sdkautomation@microsoft.com>
2025-03-31 07:04:05 +0000

    CodeGen from PR 33177 in Azure/azure-rest-api-specs Merge 3912a0f4d36fb379419f293f7ed92a02a7125845 into 45dc6ae69bfe4cc8147356fd182e423b595e79bc

--- sdk/pineconevectordb/azure-mgmt-pineconevectordb/azure/mgmt/pineconevectordb/operations/_operations.py
+++ sdk/pineconevectordb/azure-mgmt-pineconevectordb/azure/mgmt/pineconevectordb/operations/_operations.py
@@ -320,7 +320,7 @@ class Operations:
 
         def extract_data(pipeline_response):
             deserialized = pipeline_response.http_response.json()
-            list_of_elem = _deserialize(List[_models.Operation], deserialized["value"])
+            list_of_elem = _deserialize(List[_models.Operation], deserialized.get("value", []))
             if cls:
                 list_of_elem = cls(list_of_elem)  # type: ignore
             return deserialized.get("nextLink") or None, iter(list_of_elem)
@@ -1007,7 +1007,7 @@ class OrganizationsOperations:
 
         def extract_data(pipeline_response):
             deserialized = pipeline_response.http_response.json()
-            list_of_elem = _deserialize(List[_models.OrganizationResource], deserialized["value"])
+            list_of_elem = _deserialize(List[_models.OrganizationResource], deserialized.get("value", []))
             if cls:
                 list_of_elem = cls(list_of_elem)  # type: ignore
             return deserialized.get("nextLink") or None, iter(list_of_elem)
@@ -1091,7 +1091,7 @@ class OrganizationsOperations:
 
         def extract_data(pipeline_response):
             deserialized = pipeline_response.http_response.json()
-            list_of_elem = _deserialize(List[_models.OrganizationResource], deserialized["value"])
+            list_of_elem = _deserialize(List[_models.OrganizationResource], deserialized.get("value", []))
             if cls:
                 list_of_elem = cls(list_of_elem)  # type: ignore
             return deserialized.get("nextLink") or None, iter(list_of_elem)
