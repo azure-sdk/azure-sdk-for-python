666b8dd9ecd37cf167153e72fdec8da3dc995553
SDKAuto <sdkautomation@microsoft.com>
2025-04-01 21:16:29 +0000

    CodeGen from PR 33639 in Azure/azure-rest-api-specs Merge 9f20bf19f8c90a0ba5e6601b811910180399d2c7 into e53298c896c9c0b7af87d645a71b223fadea3f77

--- sdk/reservations/azure-mgmt-reservations/generated_samples/archive.py
+++ sdk/reservations/azure-mgmt-reservations/generated_samples/archive.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.reservations import AzureReservationAPI
 
 """
@@ -28,11 +29,10 @@ def main():
         credential=DefaultAzureCredential(),
     )
 
-    response = client.reservation.archive(
+    client.reservation.archive(
         reservation_order_id="276e7ae4-84d0-4da6-ab4b-d6b94f3557da",
         reservation_id="356e7ae4-84d0-4da6-ab4b-d6b94f3557da",
     )
-    print(response)
 
 
 # x-ms-original-file: specification/reservations/resource-manager/Microsoft.Capacity/stable/2022-11-01/examples/Archive.json
