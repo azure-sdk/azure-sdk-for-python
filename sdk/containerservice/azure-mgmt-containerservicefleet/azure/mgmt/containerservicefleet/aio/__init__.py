ff365f960f2e3308121cf9e6029ff55b73da9be8
SDKAuto <sdkautomation@microsoft.com>
2025-04-01 05:34:12 +0000

    CodeGen from PR 33354 in Azure/azure-rest-api-specs Merge 857b0151c33be5f987ca7dc7421251874bbc1cd7 into 375b84f6498faab98e666891024f73292db89f2f

--- sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/aio/__init__.py
+++ sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/aio/__init__.py
@@ -7,17 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._container_service_fleet_mgmt_client import ContainerServiceFleetMgmtClient
-
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *  # pylint: disable=unused-wildcard-import
-except ImportError:
-    _patch_all = []
-from ._patch import patch_sdk as _patch_sdk
-
-__all__ = [
-    "ContainerServiceFleetMgmtClient",
-]
-__all__.extend([p for p in _patch_all if p not in __all__])
-
-_patch_sdk()
+__all__ = ['ContainerServiceFleetMgmtClient']
