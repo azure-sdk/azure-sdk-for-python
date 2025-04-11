4abbbc4069a40bb23d3fc635fe5ddc753b0da2b9
SDKAuto <sdkautomation@microsoft.com>
2025-04-11 22:05:44 +0000

    CodeGen from PR 33410 in Azure/azure-rest-api-specs Merge 114af2dc013164292d8f5eaeb6d13b9a5785bea2 into ef851ba96b020132a9ddf2d3c74af5aaacc023dd

--- sdk/translation/azure-ai-translation-text/azure/ai/translation/text/aio/_client.py
+++ sdk/translation/azure-ai-translation-text/azure/ai/translation/text/aio/_client.py
@@ -8,6 +8,7 @@
 
 from copy import deepcopy
 from typing import Any, Awaitable
+from typing_extensions import Self
 
 from azure.core import AsyncPipelineClient
 from azure.core.pipeline import policies
@@ -18,7 +19,7 @@ from ._configuration import TextTranslationClientConfiguration
 from ._operations import TextTranslationClientOperationsMixin
 
 
-class TextTranslationClient(TextTranslationClientOperationsMixin):  # pylint: disable=client-accepts-api-version-keyword
+class TextTranslationClient(TextTranslationClientOperationsMixin):
     """Text translation is a cloud-based REST API feature of the Translator service that uses neural
     machine translation technology to enable quick and accurate source-to-target text translation
     in real time across all supported languages.
@@ -37,16 +38,12 @@ class TextTranslationClient(TextTranslationClientOperationsMixin):  # pylint: di
     Detect. Returns the source code language code and a boolean variable denoting whether the
     detected language is supported for text translation and transliteration.
 
-    Dictionary lookup. Returns equivalent words for the source term in the target language.
-
-    Dictionary example Returns grammatical structure and context examples for the source term and
-    target term pair.
-
     :param endpoint: Supported Text Translation endpoints (protocol and hostname, for example:
-         https://api.cognitive.microsofttranslator.com). Required.
+         `https://api.cognitive.microsofttranslator.com
+     <https://api.cognitive.microsofttranslator.com>`_). Required.
     :type endpoint: str
-    :keyword api_version: Mandatory API version parameter. Default value is "3.0". Note that
-     overriding this default value may result in unsupported behavior.
+    :keyword api_version: Mandatory API version parameter. Default value is "2025-05-01-preview".
+     Note that overriding this default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
@@ -109,7 +106,7 @@ class TextTranslationClient(TextTranslationClientOperationsMixin):  # pylint: di
     async def close(self) -> None:
         await self._client.close()
 
-    async def __aenter__(self) -> "TextTranslationClient":
+    async def __aenter__(self) -> Self:
         await self._client.__aenter__()
         return self
 
