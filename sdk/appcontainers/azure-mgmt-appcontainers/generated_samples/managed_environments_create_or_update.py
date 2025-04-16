42bb0a039f19737d25b54bdae49d1ebed4acdd3a
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 06:24:32 +0000

    CodeGen from PR 33984 in Azure/azure-rest-api-specs Merge c9454c176e109f05f8e4c6201a186291bd0133bf into 1837522938859566334cdaf6edebfe736a230c37

--- sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/managed_environments_create_or_update.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/managed_environments_create_or_update.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -52,12 +53,20 @@ def main():
                         "sharedKey": "string",
                     }
                 },
+                "availabilityZones": ["1", "2", "3"],
                 "customDomainConfiguration": {
                     "certificatePassword": "1234",
                     "certificateValue": "Y2VydA==",
                     "dnsSuffix": "www.my-name.com",
                 },
                 "daprAIConnectionString": "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://northcentralus-0.in.applicationinsights.azure.com/",
+                "ingressConfiguration": {
+                    "headerCountLimit": 30,
+                    "requestIdleTimeout": 5,
+                    "scale": {"maxReplicas": 4, "minReplicas": 2},
+                    "terminationGracePeriodSeconds": 3600,
+                    "workloadProfileName": "My-CO-01",
+                },
                 "openTelemetryConfiguration": {
                     "destinationsConfiguration": {
                         "dataDogConfiguration": {"key": "000000000000000000000000", "site": "string"},
@@ -108,6 +117,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples/ManagedEnvironments_CreateOrUpdate.json
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2025-02-02-preview/examples/ManagedEnvironments_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
--- sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/session_pools_create_or_update.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_samples/session_pools_create_or_update.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -34,6 +35,7 @@ def main():
         resource_group_name="rg",
         session_pool_name="testsessionpool",
         session_pool_envelope={
+            "identity": {"type": "SystemAssigned"},
             "location": "East US",
             "properties": {
                 "containerType": "CustomContainer",
@@ -48,9 +50,14 @@ def main():
                         }
                     ],
                     "ingress": {"targetPort": 80},
+                    "registryCredentials": {
+                        "identity": "/subscriptions/7a497526-bb8d-4816-9795-db1418a1f977/resourcegroups/test/providers/Microsoft.ManagedIdentity/userAssignedIdentities/testSP",
+                        "server": "test.azurecr.io",
+                    },
                 },
                 "dynamicPoolConfiguration": {"cooldownPeriodInSeconds": 600, "executionType": "Timed"},
                 "environmentId": "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rg/providers/Microsoft.App/managedEnvironments/demokube",
+                "managedIdentitySettings": [{"identity": "system", "lifecycle": "Main"}],
                 "poolManagementType": "Dynamic",
                 "scaleConfiguration": {"maxConcurrentSessions": 500, "readySessionInstances": 100},
                 "sessionNetworkConfiguration": {"status": "EgressEnabled"},
@@ -60,6 +67,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2024-08-02-preview/examples/SessionPools_CreateOrUpdate.json
+# x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2025-02-02-preview/examples/SessionPools_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
