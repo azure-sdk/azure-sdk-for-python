e7f46465c2bd81566ee4def2111618e9de7b68bd
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 07:16:00 +0000

    CodeGen from PR 33986 in Azure/azure-rest-api-specs Merge c0d9c680f240997b7a7d0de1eaebd9986988bc1a into 4f6ba43a83ed4f80863e1fe698ac10b89e8c48a0

--- sdk/advisor/azure-mgmt-advisor/azure/mgmt/advisor/aio/_advisor_management_client.py
+++ sdk/advisor/azure-mgmt-advisor/azure/mgmt/advisor/aio/_advisor_management_client.py
@@ -8,27 +8,37 @@
 
 from copy import deepcopy
 from typing import Any, Awaitable, TYPE_CHECKING
+from typing_extensions import Self
 
+from azure.core.pipeline import policies
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core import AsyncARMPipelineClient
+from azure.mgmt.core.policies import AsyncARMAutoResourceProviderRegistrationPolicy
 
-from .. import models
+from .. import models as _models
 from .._serialization import Deserializer, Serializer
 from ._configuration import AdvisorManagementClientConfiguration
 from .operations import (
+    AdvisorManagementClientOperationsMixin,
+    AdvisorScoresOperations,
+    AssessmentTypesOperations,
+    AssessmentsOperations,
     ConfigurationsOperations,
     Operations,
     RecommendationMetadataOperations,
     RecommendationsOperations,
+    ResiliencyReviewsOperations,
     SuppressionsOperations,
+    TriageRecommendationsOperations,
+    TriageResourcesOperations,
+    WorkloadsOperations,
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
 
-class AdvisorManagementClient:  # pylint: disable=client-accepts-api-version-keyword
+class AdvisorManagementClient(AdvisorManagementClientOperationsMixin):  # pylint: disable=too-many-instance-attributes
     """REST APIs for Azure Advisor.
 
     :ivar recommendation_metadata: RecommendationMetadataOperations operations
@@ -42,14 +52,29 @@ class AdvisorManagementClient:  # pylint: disable=client-accepts-api-version-key
     :vartype operations: azure.mgmt.advisor.aio.operations.Operations
     :ivar suppressions: SuppressionsOperations operations
     :vartype suppressions: azure.mgmt.advisor.aio.operations.SuppressionsOperations
+    :ivar advisor_scores: AdvisorScoresOperations operations
+    :vartype advisor_scores: azure.mgmt.advisor.aio.operations.AdvisorScoresOperations
+    :ivar assessments: AssessmentsOperations operations
+    :vartype assessments: azure.mgmt.advisor.aio.operations.AssessmentsOperations
+    :ivar assessment_types: AssessmentTypesOperations operations
+    :vartype assessment_types: azure.mgmt.advisor.aio.operations.AssessmentTypesOperations
+    :ivar workloads: WorkloadsOperations operations
+    :vartype workloads: azure.mgmt.advisor.aio.operations.WorkloadsOperations
+    :ivar resiliency_reviews: ResiliencyReviewsOperations operations
+    :vartype resiliency_reviews: azure.mgmt.advisor.aio.operations.ResiliencyReviewsOperations
+    :ivar triage_recommendations: TriageRecommendationsOperations operations
+    :vartype triage_recommendations:
+     azure.mgmt.advisor.aio.operations.TriageRecommendationsOperations
+    :ivar triage_resources: TriageResourcesOperations operations
+    :vartype triage_resources: azure.mgmt.advisor.aio.operations.TriageResourcesOperations
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The Azure subscription ID. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
-    :keyword api_version: Api Version. Default value is "2020-01-01". Note that overriding this
-     default value may result in unsupported behavior.
+    :keyword api_version: Api Version. Default value is "2024-11-18-preview". Note that overriding
+     this default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
@@ -63,9 +88,27 @@ class AdvisorManagementClient:  # pylint: disable=client-accepts-api-version-key
         self._config = AdvisorManagementClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                AsyncARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
 
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
+        client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
@@ -76,8 +119,25 @@ class AdvisorManagementClient:  # pylint: disable=client-accepts-api-version-key
         self.recommendations = RecommendationsOperations(self._client, self._config, self._serialize, self._deserialize)
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
         self.suppressions = SuppressionsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.advisor_scores = AdvisorScoresOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.assessments = AssessmentsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.assessment_types = AssessmentTypesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.workloads = WorkloadsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.resiliency_reviews = ResiliencyReviewsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.triage_recommendations = TriageRecommendationsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.triage_resources = TriageResourcesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
+    def _send_request(
+        self, request: HttpRequest, *, stream: bool = False, **kwargs: Any
+    ) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -97,14 +157,14 @@ class AdvisorManagementClient:  # pylint: disable=client-accepts-api-version-key
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     async def close(self) -> None:
         await self._client.close()
 
-    async def __aenter__(self) -> "AdvisorManagementClient":
+    async def __aenter__(self) -> Self:
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
