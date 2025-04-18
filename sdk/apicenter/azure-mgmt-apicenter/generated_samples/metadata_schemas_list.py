08abd042112d8ed52d4e2a62990f215c3633668c
SDKAuto <sdkautomation@microsoft.com>
2025-04-18 02:11:09 +0000

    CodeGen from PR 34006 in Azure/azure-rest-api-specs Merge 6cc659de67576bdedf53855ec4dcad2ed42a03ed into 877f166d9819e61625338a9ebb9922e1fc646636

--- sdk/apicenter/azure-mgmt-apicenter/generated_samples/metadata_schemas_list.py
+++ sdk/apicenter/azure-mgmt-apicenter/generated_samples/metadata_schemas_list.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.apicenter import ApiCenterMgmtClient
 
 """
@@ -37,6 +39,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/apicenter/resource-manager/Microsoft.ApiCenter/stable/2024-03-01/examples/MetadataSchemas_List.json
+# x-ms-original-file: specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2024-06-01-preview/examples/MetadataSchemas_List.json
 if __name__ == "__main__":
     main()
