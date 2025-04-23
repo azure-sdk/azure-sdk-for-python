463fcfe2c0e1439bb0725b5c68b0edb0b674b5e7
SDKAuto <sdkautomation@microsoft.com>
2025-04-23 21:21:41 +0000

    CodeGen from PR 34176 in Azure/azure-rest-api-specs Merge e3e76f495f9601d69943235532c1f3f9e1da5857 into 6554cf7dbac276739c1c2d33f7d8533e35db1eaa

--- sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_custom_ca_trust_certificates.py
+++ sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_custom_ca_trust_certificates.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -73,6 +74,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2025-02-01/examples/ManagedClustersCreate_CustomCATrustCertificates.json
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2025-03-01/examples/ManagedClustersCreate_CustomCATrustCertificates.json
 if __name__ == "__main__":
     main()
