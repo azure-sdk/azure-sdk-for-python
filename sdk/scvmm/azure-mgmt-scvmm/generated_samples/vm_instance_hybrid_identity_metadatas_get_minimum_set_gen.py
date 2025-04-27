41baeb5720a23ea0cf87983bd3f54b4c300b6f01
SDKAuto <sdkautomation@microsoft.com>
2025-04-27 04:17:09 +0000

    CodeGen from PR 34242 in Azure/azure-rest-api-specs Merge 8fbc11097a7044c1adc129e989a96f3988cf536d into 11059b2f00c7572b276dc9862c0b41db8702cc78

--- sdk/scvmm/azure-mgmt-scvmm/generated_samples/vm_instance_hybrid_identity_metadatas_get_minimum_set_gen.py
+++ sdk/scvmm/azure-mgmt-scvmm/generated_samples/vm_instance_hybrid_identity_metadatas_get_minimum_set_gen.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -36,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/VmInstanceHybridIdentityMetadatas_Get_MinimumSet_Gen.json
+# x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2025-03-13/examples/VmInstanceHybridIdentityMetadatas_Get_MinimumSet_Gen.json
 if __name__ == "__main__":
     main()
