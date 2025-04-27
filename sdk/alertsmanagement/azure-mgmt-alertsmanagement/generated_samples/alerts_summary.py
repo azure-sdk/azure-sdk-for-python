8683c754ea97e501b8f3cc5d23ca8c216d9b6fb0
SDKAuto <sdkautomation@microsoft.com>
2025-04-27 07:49:24 +0000

    CodeGen from PR 33506 in Azure/azure-rest-api-specs Merge 1bc8ec2a71e393ca0f833c14be1cd85a7865ca71 into 11059b2f00c7572b276dc9862c0b41db8702cc78

--- sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alerts_summary.py
+++ sdk/alertsmanagement/azure-mgmt-alertsmanagement/generated_samples/alerts_summary.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.alertsmanagement import AlertsManagementClient
 
 """
@@ -26,15 +28,17 @@ from azure.mgmt.alertsmanagement import AlertsManagementClient
 def main():
     client = AlertsManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id="1e3ff1c0-771a-4119-a03b-be82a51e232d",
+        issue_name="ISSUE_NAME",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
     response = client.alerts.get_summary(
+        scope="subscriptions/1e3ff1c0-771a-4119-a03b-be82a51e232d",
         groupby="severity,alertState",
     )
     print(response)
 
 
-# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/examples/Alerts_Summary.json
+# x-ms-original-file: specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2024-01-01-preview/examples/Alerts_Summary.json
 if __name__ == "__main__":
     main()
