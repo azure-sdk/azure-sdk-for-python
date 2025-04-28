44cb0fa4d08ee1e70f9d119075f3e63bae3423d0
SDKAuto <sdkautomation@microsoft.com>
2025-04-28 19:48:28 +0000

    CodeGen from PR 34098 in Azure/azure-rest-api-specs Merge 508ef22e81e2569e055c44ee5737ff6039a33d54 into 48ffa8bbff8cd9cb32440ee5047d4e19b6771a5d

--- sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_azure_service_mesh.py
+++ sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_azure_service_mesh.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -54,9 +55,8 @@ def main():
                     }
                 ],
                 "autoScalerProfile": {"scale-down-delay-after-add": "15m", "scan-interval": "20s"},
-                "diskEncryptionSetID": "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg1/providers/Microsoft.Compute/diskEncryptionSets/des",
+                "diskEncryptionSetID": "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Compute/diskEncryptionSets/des",
                 "dnsPrefix": "dnsprefix1",
-                "enablePodSecurityPolicy": True,
                 "enableRBAC": True,
                 "kubernetesVersion": "",
                 "linuxProfile": {"adminUsername": "azureuser", "ssh": {"publicKeys": [{"keyData": "keydata"}]}},
@@ -77,7 +77,7 @@ def main():
                             }
                         },
                         "components": {
-                            "egressGateways": [{"enabled": True}],
+                            "egressGateways": [{"enabled": True, "name": "istioegress1"}],
                             "ingressGateways": [{"enabled": True, "mode": "Internal"}],
                         },
                     },
@@ -93,6 +93,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2025-02-01/examples/ManagedClustersCreate_AzureServiceMesh.json
+# x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2025-03-02-preview/examples/ManagedClustersCreate_AzureServiceMesh.json
 if __name__ == "__main__":
     main()
