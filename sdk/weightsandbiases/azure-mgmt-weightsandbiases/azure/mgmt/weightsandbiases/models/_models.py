0b39a5c7118cbbe54584aafc03b940005e5dcbda
SDKAuto <sdkautomation@microsoft.com>
2025-05-09 02:46:11 +0000

    CodeGen from PR 34492 in Azure/azure-rest-api-specs Merge 996fce95c4a7d354249d25636b36adfc09f2804c into 9f69db7532b6706baedc004f2eda762196a3e1eb

--- sdk/weightsandbiases/azure-mgmt-weightsandbiases/azure/mgmt/weightsandbiases/models/_models.py
+++ sdk/weightsandbiases/azure-mgmt-weightsandbiases/azure/mgmt/weightsandbiases/models/_models.py
@@ -11,14 +11,13 @@
 import datetime
 from typing import Any, Dict, List, Mapping, Optional, TYPE_CHECKING, Union, overload
 
-from .. import _model_base
-from .._model_base import rest_field
+from .._utils.model_base import Model as _Model, rest_field
 
 if TYPE_CHECKING:
     from .. import models as _models
 
 
-class ErrorAdditionalInfo(_model_base.Model):
+class ErrorAdditionalInfo(_Model):
     """The resource management error additional info.
 
     :ivar type: The additional info type.
@@ -33,7 +32,7 @@ class ErrorAdditionalInfo(_model_base.Model):
     """The additional info."""
 
 
-class ErrorDetail(_model_base.Model):
+class ErrorDetail(_Model):
     """The error detail.
 
     :ivar code: The error code.
@@ -62,9 +61,8 @@ class ErrorDetail(_model_base.Model):
     """The error additional info."""
 
 
-class ErrorResponse(_model_base.Model):
-    """Common error response for all Azure Resource Manager APIs to return error details for failed
-    operations.
+class ErrorResponse(_Model):
+    """Error response.
 
     :ivar error: The error object.
     :vartype error: ~azure.mgmt.weightsandbiases.models.ErrorDetail
@@ -91,7 +89,7 @@ class ErrorResponse(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class InstanceProperties(_model_base.Model):
+class InstanceProperties(_Model):
     """Properties specific to Instance.
 
     :ivar marketplace: Marketplace details of the resource. Required.
@@ -102,7 +100,7 @@ class InstanceProperties(_model_base.Model):
      "Failed", and "Canceled".
     :vartype provisioning_state: str or
      ~azure.mgmt.weightsandbiases.models.ResourceProvisioningState
-    :ivar partner_properties: partner properties.
+    :ivar partner_properties: partner properties. Required.
     :vartype partner_properties: ~azure.mgmt.weightsandbiases.models.PartnerProperties
     :ivar single_sign_on_properties: Single sign-on properties.
     :vartype single_sign_on_properties:
@@ -118,10 +116,10 @@ class InstanceProperties(_model_base.Model):
     )
     """Provisioning state of the resource. Known values are: \"Succeeded\", \"Failed\", and
      \"Canceled\"."""
-    partner_properties: Optional["_models.PartnerProperties"] = rest_field(
+    partner_properties: "_models.PartnerProperties" = rest_field(
         name="partnerProperties", visibility=["read", "create", "update", "delete", "query"]
     )
-    """partner properties."""
+    """partner properties. Required."""
     single_sign_on_properties: Optional["_models.SingleSignOnPropertiesV2"] = rest_field(
         name="singleSignOnProperties", visibility=["read", "create", "update", "delete", "query"]
     )
@@ -133,7 +131,7 @@ class InstanceProperties(_model_base.Model):
         *,
         marketplace: "_models.MarketplaceDetails",
         user: "_models.UserDetails",
-        partner_properties: Optional["_models.PartnerProperties"] = None,
+        partner_properties: "_models.PartnerProperties",
         single_sign_on_properties: Optional["_models.SingleSignOnPropertiesV2"] = None,
     ) -> None: ...
 
@@ -148,8 +146,8 @@ class InstanceProperties(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class Resource(_model_base.Model):
-    """Common fields that are returned in the response for all Azure Resource Manager resources.
+class Resource(_Model):
+    """Resource.
 
     :ivar id: Fully qualified resource ID for the resource. Ex -
      /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}.
@@ -177,8 +175,7 @@ class Resource(_model_base.Model):
 
 
 class TrackedResource(Resource):
-    """The resource model definition for an Azure Resource Manager tracked top level resource which
-    has 'tags' and a 'location'.
+    """Tracked Resource.
 
     :ivar id: Fully qualified resource ID for the resource. Ex -
      /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}.
@@ -276,7 +273,7 @@ class InstanceResource(TrackedResource):
         super().__init__(*args, **kwargs)
 
 
-class InstanceResourceUpdate(_model_base.Model):
+class InstanceResourceUpdate(_Model):
     """The type used for update operations of the Instance Resource.
 
     :ivar tags: Resource tags.
@@ -311,7 +308,7 @@ class InstanceResourceUpdate(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class ManagedServiceIdentity(_model_base.Model):
+class ManagedServiceIdentity(_Model):
     """Managed service identity (system assigned and/or user assigned identities).
 
     :ivar principal_id: The service principal ID of the system assigned identity. This property
@@ -363,7 +360,7 @@ class ManagedServiceIdentity(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class MarketplaceDetails(_model_base.Model):
+class MarketplaceDetails(_Model):
     """Marketplace details for an organization.
 
     :ivar subscription_id: Azure subscription id for the the marketplace offer is purchased from.
@@ -409,7 +406,7 @@ class MarketplaceDetails(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class OfferDetails(_model_base.Model):
+class OfferDetails(_Model):
     """Offer details for the marketplace that is selected by the user.
 
     :ivar publisher_id: Publisher Id for the marketplace offer. Required.
@@ -462,8 +459,8 @@ class OfferDetails(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class Operation(_model_base.Model):
-    """Details of a REST API operation, returned from the Resource Provider Operations API.
+class Operation(_Model):
+    """REST API Operation.
 
     :ivar name: The name of the operation, as per Resource-Based Access Control (RBAC). Examples:
      "Microsoft.Compute/virtualMachines/write", "Microsoft.Compute/virtualMachines/capture/action".
@@ -519,7 +516,7 @@ class Operation(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class OperationDisplay(_model_base.Model):
+class OperationDisplay(_Model):
     """Localized display information for and operation.
 
     :ivar provider: The localized friendly form of the resource provider name, e.g. "Microsoft
@@ -550,7 +547,7 @@ class OperationDisplay(_model_base.Model):
      views."""
 
 
-class PartnerProperties(_model_base.Model):
+class PartnerProperties(_Model):
     """Partner's specific Properties.
 
     :ivar region: The region of the instance. Required. Known values are: "eastus", "centralus",
@@ -585,7 +582,7 @@ class PartnerProperties(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class SingleSignOnPropertiesV2(_model_base.Model):
+class SingleSignOnPropertiesV2(_Model):
     """Properties specific to Single Sign On Resource.
 
     :ivar type: Type of Single Sign-On mechanism being used. Required. Known values are: "Saml" and
@@ -645,7 +642,7 @@ class SingleSignOnPropertiesV2(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class SystemData(_model_base.Model):
+class SystemData(_Model):
     """Metadata pertaining to creation and last modification of the resource.
 
     :ivar created_by: The identity that created the resource.
@@ -712,22 +709,22 @@ class SystemData(_model_base.Model):
         super().__init__(*args, **kwargs)
 
 
-class UserAssignedIdentity(_model_base.Model):
+class UserAssignedIdentity(_Model):
     """User assigned identity properties.
 
-    :ivar client_id: The client ID of the assigned identity.
-    :vartype client_id: str
     :ivar principal_id: The principal ID of the assigned identity.
     :vartype principal_id: str
+    :ivar client_id: The client ID of the assigned identity.
+    :vartype client_id: str
     """
 
-    client_id: Optional[str] = rest_field(name="clientId", visibility=["read"])
-    """The client ID of the assigned identity."""
     principal_id: Optional[str] = rest_field(name="principalId", visibility=["read"])
     """The principal ID of the assigned identity."""
+    client_id: Optional[str] = rest_field(name="clientId", visibility=["read"])
+    """The client ID of the assigned identity."""
 
 
-class UserDetails(_model_base.Model):
+class UserDetails(_Model):
     """User details for an organization.
 
     :ivar first_name: First name of the user.
