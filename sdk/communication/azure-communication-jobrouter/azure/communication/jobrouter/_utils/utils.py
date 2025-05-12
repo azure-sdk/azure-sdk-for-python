14fd51eb62780cf731be37562331e56113037bd9
SDKAuto <sdkautomation@microsoft.com>
2025-05-12 19:41:58 +0000

    CodeGen from PR 34508 in Azure/azure-rest-api-specs Merge 1a7056622a25f9802a9007200b9e8790376ebdba into f9582b6167e2c81decc4d6b54950afe585e2e610

--- sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/_utils/utils.py
+++ sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/_utils/utils.py
@@ -6,33 +6,23 @@
 # --------------------------------------------------------------------------
 
 from abc import ABC
-from typing import Optional, TYPE_CHECKING
+from typing import Generic, Optional, TYPE_CHECKING, TypeVar
 
 from azure.core import MatchConditions
 
-from ._configuration import JobRouterAdministrationClientConfiguration, JobRouterClientConfiguration
-
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
-    from azure.core import PipelineClient
-
-    from ._serialization import Deserializer, Serializer
+    from .serialization import Deserializer, Serializer
 
 
-class JobRouterAdministrationClientMixinABC(ABC):
+TClient = TypeVar("TClient")
+TConfig = TypeVar("TConfig")
+
+
+class ClientMixinABC(ABC, Generic[TClient, TConfig]):
     """DO NOT use this class. It is for internal typing use only."""
 
-    _client: "PipelineClient"
-    _config: JobRouterAdministrationClientConfiguration
-    _serialize: "Serializer"
-    _deserialize: "Deserializer"
-
-
-class JobRouterClientMixinABC(ABC):
-    """DO NOT use this class. It is for internal typing use only."""
-
-    _client: "PipelineClient"
-    _config: JobRouterClientConfiguration
+    _client: TClient
+    _config: TConfig
     _serialize: "Serializer"
     _deserialize: "Deserializer"
 
