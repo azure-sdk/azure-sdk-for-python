c7f36d5726319481a6d39092ba9c81f2d5b9a425
SDKAuto <sdkautomation@microsoft.com>
2025-05-12 19:02:07 +0000

    CodeGen from PR 33843 in Azure/azure-rest-api-specs Merge dc6155eb6a0be0e422c89ae90247037564c0a070 into e489de297e262f087de27000a48dd69417e345a4

--- sdk/security/azure-mgmt-security/azure/mgmt/security/v2019_01_01/models/_models_py3.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/v2019_01_01/models/_models_py3.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# pylint: disable=too-many-lines
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
@@ -9,7 +8,7 @@
 
 from typing import Any, Optional
 
-from ... import _serialization
+from .._utils import serialization as _serialization
 
 
 class Resource(_serialization.Model):
@@ -40,9 +39,9 @@ class Resource(_serialization.Model):
     def __init__(self, **kwargs: Any) -> None:
         """ """
         super().__init__(**kwargs)
-        self.id = None
-        self.name = None
-        self.type = None
+        self.id: Optional[str] = None
+        self.name: Optional[str] = None
+        self.type: Optional[str] = None
 
 
 class AdvancedThreatProtectionSetting(Resource):
@@ -118,11 +117,11 @@ class CloudErrorBody(_serialization.Model):
     def __init__(self, **kwargs: Any) -> None:
         """ """
         super().__init__(**kwargs)
-        self.code = None
-        self.message = None
-        self.target = None
-        self.details = None
-        self.additional_info = None
+        self.code: Optional[str] = None
+        self.message: Optional[str] = None
+        self.target: Optional[str] = None
+        self.details: Optional[List["_models.CloudErrorBody"]] = None
+        self.additional_info: Optional[List["_models.ErrorAdditionalInfo"]] = None
 
 
 class ErrorAdditionalInfo(_serialization.Model):
@@ -149,5 +148,5 @@ class ErrorAdditionalInfo(_serialization.Model):
     def __init__(self, **kwargs: Any) -> None:
         """ """
         super().__init__(**kwargs)
-        self.type = None
-        self.info = None
+        self.type: Optional[str] = None
+        self.info: Optional[JSON] = None
--- sdk/security/azure-mgmt-security/azure/mgmt/security/v2024_04_01/aio/_security_center.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/v2024_04_01/aio/_security_center.py
@@ -7,15 +7,18 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Awaitable, TYPE_CHECKING
+from typing import Any, Awaitable, Optional, TYPE_CHECKING, cast
+from typing_extensions import Self
 
 from azure.core.pipeline import policies
 from azure.core.rest import AsyncHttpResponse, HttpRequest
+from azure.core.settings import settings
 from azure.mgmt.core import AsyncARMPipelineClient
 from azure.mgmt.core.policies import AsyncARMAutoResourceProviderRegistrationPolicy
+from azure.mgmt.core.tools import get_arm_endpoints
 
 from .. import models as _models
-from ..._serialization import Deserializer, Serializer
+from .._utils.serialization import Deserializer, Serializer
 from ._configuration import SecurityCenterConfiguration
 from .operations import (
     AzureDevOpsOrgsOperations,
@@ -31,11 +34,10 @@ from .operations import (
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
 
-class SecurityCenter:  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
+class SecurityCenter:  # pylint: disable=too-many-instance-attributes
     """API spec for Microsoft.Security (Azure Security Center) resource provider.
 
     :ivar azure_dev_ops_orgs: AzureDevOpsOrgsOperations operations
@@ -69,7 +71,7 @@ class SecurityCenter:  # pylint: disable=client-accepts-api-version-keyword,too-
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The ID of the target subscription. Required.
     :type subscription_id: str
-    :param base_url: Service URL. Default value is "https://management.azure.com".
+    :param base_url: Service URL. Default value is None.
     :type base_url: str
     :keyword api_version: Api Version. Default value is "2024-04-01". Note that overriding this
      default value may result in unsupported behavior.
@@ -79,13 +81,17 @@ class SecurityCenter:  # pylint: disable=client-accepts-api-version-keyword,too-
     """
 
     def __init__(
-        self,
-        credential: "AsyncTokenCredential",
-        subscription_id: str,
-        base_url: str = "https://management.azure.com",
-        **kwargs: Any
+        self, credential: "AsyncTokenCredential", subscription_id: str, base_url: Optional[str] = None, **kwargs: Any
     ) -> None:
-        self._config = SecurityCenterConfiguration(credential=credential, subscription_id=subscription_id, **kwargs)
+        _cloud = kwargs.pop("cloud_setting", None) or settings.current.azure_cloud  # type: ignore
+        _endpoints = get_arm_endpoints(_cloud)
+        if not base_url:
+            base_url = _endpoints["resource_manager"]
+        credential_scopes = kwargs.pop("credential_scopes", _endpoints["credential_scopes"])
+        self._config = SecurityCenterConfiguration(
+            credential=credential, subscription_id=subscription_id, credential_scopes=credential_scopes, **kwargs
+        )
+
         _policies = kwargs.pop("policies", None)
         if _policies is None:
             _policies = [
@@ -104,7 +110,9 @@ class SecurityCenter:  # pylint: disable=client-accepts-api-version-keyword,too-
                 policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
                 self._config.http_logging_policy,
             ]
-        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(
+            base_url=cast(str, base_url), policies=_policies, **kwargs
+        )
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -168,7 +176,7 @@ class SecurityCenter:  # pylint: disable=client-accepts-api-version-keyword,too-
     async def close(self) -> None:
         await self._client.close()
 
-    async def __aenter__(self) -> "SecurityCenter":
+    async def __aenter__(self) -> Self:
         await self._client.__aenter__()
         return self
 
