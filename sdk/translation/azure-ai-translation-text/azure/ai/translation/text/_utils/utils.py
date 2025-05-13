7b40782d813ef939bc484f685e632137c36e77ec
SDKAuto <sdkautomation@microsoft.com>
2025-05-13 07:01:16 +0000

    CodeGen from PR 33410 in Azure/azure-rest-api-specs Merge 1b94ded8fbc174386df38e49fdeacaa4a057677a into ba0c086df0ebe03a61579485c1c10de0d17804b2

--- sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_utils/utils.py
+++ sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_utils/utils.py
@@ -6,24 +6,23 @@
 # --------------------------------------------------------------------------
 
 from abc import ABC
-from typing import Optional, TYPE_CHECKING
+from typing import Generic, Optional, TYPE_CHECKING, TypeVar
 
 from azure.core import MatchConditions
 
-from ._configuration import TextTranslationClientConfiguration
-
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
-    from azure.core import AsyncPipelineClient
-
-    from .._serialization import Deserializer, Serializer
+    from .serialization import Deserializer, Serializer
 
 
-class TextTranslationClientMixinABC(ABC):
+TClient = TypeVar("TClient")
+TConfig = TypeVar("TConfig")
+
+
+class ClientMixinABC(ABC, Generic[TClient, TConfig]):
     """DO NOT use this class. It is for internal typing use only."""
 
-    _client: "AsyncPipelineClient"
-    _config: TextTranslationClientConfiguration
+    _client: TClient
+    _config: TConfig
     _serialize: "Serializer"
     _deserialize: "Deserializer"
 
