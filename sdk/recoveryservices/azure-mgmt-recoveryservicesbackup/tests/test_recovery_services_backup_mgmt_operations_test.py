936b4c6c32dff2abda07ed2e1bf7af2333ee9899
ChenxiJiang333 <v-chenjiang@microsoft.com>
2026-02-27 16:36:18 +0800

    update

--- sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/tests/test_recovery_services_backup_mgmt_operations_test.py
+++ sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/tests/test_recovery_services_backup_mgmt_operations_test.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 import pytest
-from azure.mgmt.recoveryservicesbackup import RecoveryServicesClient
+from azure.mgmt.recoveryservicesbackup import RecoveryServicesBackupClient
 
 from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
 
@@ -16,7 +16,7 @@ AZURE_LOCATION = "eastus"
 @pytest.mark.live_test_only
 class TestStandbyPoolMgmtOperationsAsync(AzureMgmtRecordedTestCase):
     def setup_method(self, method):
-        self.client = self.create_mgmt_client(RecoveryServicesClient, is_async=True)
+        self.client = self.create_mgmt_client(RecoveryServicesBackupClient, is_async=True)
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy
